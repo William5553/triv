b2daba5d0e8d2930954f91fffcebe1c08ea1ac7b
William K <random@williamk.ga>
2020-10-31 16:33:54 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,12 @@
 const settings = require('../settings.json');
-exports.run = (client, message, params) => {
-  if (!params[0]) {
+exports.run = (client, message, args) => {
+  if (!args[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     message.channel.send('Help sent to your DMs! :mailbox_with_mail:');
     message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
   } else {
-    let command = params[0];
+    let command = args
     if (client.commands.has(command)) {
       command = client.commands.get(command);
       message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`, {code:'asciidoc'});
