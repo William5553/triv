a58e9cfc862e580efedd6dff866573c8057061e9
William K <random@williamk.ga>
2020-10-30 22:41:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ disableMentions: everyone });
+const client = new Discord.Client({ disableEveryone: true });
 const fs = require('fs');
 const request = require('request'); // eslint-disable-line no-unused-vars
 const settings = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
