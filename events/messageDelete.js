e4690f192e83532311420cda0303fb718c47c9e3
William K <random@williamk.ga>
2020-10-31 18:29:38 -0400

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,22 +1,16 @@
-const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 module.exports = message => {
-  if (message.channel.type !== "text") return;
+  if (!message.guild) return;
   const logs = message.guild.channels.cache.find(channel => channel.name === "bot-logs");
   if (message.guild.me.hasPermission("MANAGE_CHANNELS") && !logs) {
     message.guild.channels.create("bot-logs", { type: "text" });
   }
-  const entry = message.guild.fetchAuditLogs({ type: "MESSAGE_DELETE" }).then(audit => audit.entries.first());
-  let user = "";
-  if (entry.createdTimestamp > Date.now() - 5000 && entry.extra.count >= 1) {
-    user = entry.executor.username;
-  } else {
-    user = message.author.username;
-  }
-  const msgDel = new Discord.MessageEmbed()
+
+  const embed = new MessageEmbed()
     .setTitle("Message Deleted")
-    .setAuthor(message.author.username, message.author.avatarURL())
-    .addField("Message", message)
-    .addField("Deleted By", user)
-    .setColor("0x00AAFF");
-  logs.send({ msgDel });
+    .setAuthor(`${message.author.tag} - #${message.channel.name}`, message.author.avatarURL())
+    .setTimestamp()
+    .setDescription(message.content)
+    .setColor("0xEB5234");
+  if (logs) logs.send(embed);
 };
