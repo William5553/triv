c1c91c88817cec9a1d8270619303bff42d65e69f
William K <random@williamk.ga>
2020-10-31 14:40:08 -0400

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,13 +2,12 @@ exports.run = async (client, message, args) => {
   const settings = require('../settings.json');
   if (message.author.id !== settings.ownerid) return message.reply('no');
   var code = args.join(' ');
-  if (code.length < 1) return message.reply('Please input code, master.');
+  if (code.length < 1) return message.reply('tell me what to run moron');
   try {
     var evaled = eval(code);
     if (evaled && evaled.constructor.name == 'Promise')
       evaled = await evaled;
-    if (typeof evaled !== 'string')
-      evaled = require('util').inspect(evaled);
+    if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
     message.channel.send(`\`\`\`xl\n${clean(client, evaled)}\n\`\`\``
     );
   }
@@ -36,7 +35,7 @@ function clean(client, text) {
   if (typeof(text) === 'string') {
     return text.replace(/`/g, '`' + String.fromCharCode(8203))
       .replace(/@/g, '@' + String.fromCharCode(8203))
-      .replace(client.token, 'CENSORED');
+      .replace(client.token, 'no token');
   }
   else {
     return text;
