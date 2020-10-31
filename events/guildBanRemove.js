cf319414905dcadb236a8ee375886470d60a120e
William K <random@williamk.ga>
2020-10-31 00:20:10 -0400

    Update guildBanRemove.js

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports = (guild, user) => {
   const message = require('./message.js');
   message.channel.send(`${user.tag} just got unbanned!`);
-  const embed = new Discord.RichEmbed()
+  const embed = new Discord.MessageEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
     .setDescription(`**Action:** Unban\n**Target:** ${user.tag}\n**Moderator:** ${guild.client.unbanAuth.tag}\n**Reason:** ${guild.client.unbanReason}`);
