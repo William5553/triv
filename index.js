47a1e3a2237ed95944cf6fcd383d47ea71aedaaf
William K <random@williamk.ga>
2020-10-31 14:30:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,21 +45,4 @@ client.elevation = message => {
   return permlvl;
 };
 
-client.on('messageDelete', message => {
-  if (message.channel.type === 'text') {
-    var logger = message.guild.channels.find(
-      channel => channel.name === 'bot-logs'
-    );
-    if (logger) {
-      const msgDel = new Discord.MessageEmbed()
-        .setTitle('Message Deleted')
-        .addField('Author', message.author.username)
-        .addField('Message', message.cleanContent)
-        .setThumbnail(message.author.avatarURL())
-        .setColor('0x00AAFF');
-      logger.send({ msgDel });
-    }
-  }
-});
-
 client.login(settings.token);
