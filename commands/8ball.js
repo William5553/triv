2c656d585f20ae6620f8b9c59f7596e81dc6adfc
William K <random@williamk.ga>
2020-10-31 14:52:50 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,13 +1,10 @@
 exports.run = (client, message, args) => {
   const question = args.slice(0).join(' ');
-
   const responses = [
     'Explode', 'It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', ' As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'
   ];
 
-
-
-  if (question.length < 1) return message.reply('How am I going to answer nothing?!');
+  if (question.length < 1) return message.reply('ask me a question moron');
   const response = `${responses[Math.floor(Math.random() * responses.length)]}`;
   if (response === 'Explode') return message.channel.send({files: ['../assets/explode8ball.png']});
   message.channel.send(`${responses[Math.floor(Math.random() * responses.length)]}`);
