ea06750a5c79c10064fc7a8e1b84d30526c1c38e
William K <random@williamk.ga>
2020-10-30 22:54:56 -0400

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,11 +1,11 @@
-//omg no stop eslint
-
 exports.run = (client, message, args) => {
   let reason = args.slice(1).join(' ');
   client.unbanReason = reason;
   client.unbanAuth = message.author;
   let user = args[0];
-  let modlog = client.channels.find('name', 'bot-logs');
+  let modlog = message.guild.channels.find(
+      channel => channel.name === 'bot-logs'
+    );
   if (!modlog) return message.reply('I cannot find a bot-logs channel');
   if (!user) return message.reply('You must supply a User Resolvable, aka a user ID.').catch(console.error);
   if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
