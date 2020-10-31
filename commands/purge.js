e594cc527d39b4f793015e46750c8e6d56e8ffe0
William K <random@williamk.ga>
2020-10-31 17:25:12 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -15,7 +15,7 @@ exports.run = (client, message, args) => {
     if (mgct > 99) mgct = 100;
     // Deleting the messages
     message.channel.bulkDelete(mgct)
-      .catch(error => message.channel.send(`Error: **${error}**`));
+      .catch(error => return message.channel.send(`Error: **${error}**`));
     const embed = new MessageEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL())
     .setColor(0xEB5234)
