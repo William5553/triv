534f9c8fe67f56a8a901a4c3d8ec0e9794f82c90
William K <random@williamk.ga>
2020-10-30 22:11:52 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
-const chalk = require('chalk');
+const chalk = rconst chalk = require('chalk');
 module.exports = client => {
   console.log(chalk.bgGreen.black('READY!'));
   if (client.guilds.size >= 2) {
-    client.user.setGame(`~help | ${client.guilds.size} guilds!`);
+    client.user.setActivity(`~help | ${client.guilds.size} guilds`, { type: 'LISTENING'});
   } else {
-    client.user.setGame(`~help | ${client.guilds.size} guild!`);
+    client.user.setActivity(`~help | ${client.guilds.size} guild`, { type: 'LISTENING'});
   }
 };
