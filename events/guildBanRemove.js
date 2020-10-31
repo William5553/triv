bddc1f8163034f2b38867ee383136d2d895659f3
William K <random@williamk.ga>
2020-10-31 15:38:56 -0400

    Update guildBanRemove.js

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -7,6 +7,5 @@ module.exports = (guild, user) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setDescription(`**Action:** Unban\n**Target:** ${user.tag}\n**Moderator:** ${guild.client.unbanAuth.tag}\n**Reason:** ${guild.client.unbanReason}`);
-  return guild.channels.get(message.guild.channels.find(channel => channel.name === 'bot-logs').id).send({embed});
-
+  return guild.channels.cache.find(channel => channel.name === 'bot-logs').id).send({embed});
 };
