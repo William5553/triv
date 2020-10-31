45ec27aabbf3638b063fd1b5417845d29961970c
William K <random@williamk.ga>
2020-10-31 19:44:39 -0400

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -16,7 +16,6 @@ exports.run = (client, message, args) => {
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
 
     return queue.textChannel.send(`Volume set to: **${args[0]}%**`).catch(console.error);
-  }
 };
 
 exports.conf = {
