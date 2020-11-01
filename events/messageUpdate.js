24f74b87b1df6c013f5f6e31b2ebdd02350f9178
William K <random@williamk.ga>
2020-11-01 12:08:22 -0500

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,9 +1,9 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = (oldMessage, newMessage) => {
-  if (!message.guild) return;
-  const logs = message.guild.channels.cache.find(channel => channel.name === "bot-logs");
-  if (message.guild.me.hasPermission("MANAGE_CHANNELS") && !logs) {
-    message.guild.channels.create("bot-logs", { type: "text" });
+  if (!newMessage.guild) return;
+  const logs = newMessage.guild.channels.cache.find(channel => channel.name === "bot-logs");
+  if (newMessage.guild.me.hasPermission("MANAGE_CHANNELS") && !logs) {
+    newMessage.guild.channels.create("bot-logs", { type: "text" });
   }
 
   const embed = new MessageEmbed()
