455a22e8dfed72b1c18995d3d5175d8903df942a
William K <random@williamk.ga>
2020-11-01 12:35:20 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,10 @@
 
 exports.run = (client, message) => {
-  let options = ['pong', 'No! I say ping!', 'Can you say pong?', 'do I have to say pong?', 'can I say nothing?', 'Pongity Pow!', 'no!', 'meowwwww', 'erggggghhhh', `banned @${message.author}`];
+  let options = ['pong', 'No! I say ping!', 'Can you say pong?', 'do I have to say pong?', 'can I say nothing?', 'Pongity Pow!', 'no!', 'meowwwww', 'erggggghhhh', `banned ${message.author}`];
   let pingmsg = `${options[Math.floor(Math.random() * options.length)]}`;
   message.channel.send('Ping?')
     .then(msg => {
-      msg.edit(pingmsg + `(took: ${msg.createdTimestamp - message.createdTimestamp}ms)`);
+      msg.edit(pingmsg + ` (took: ${msg.createdTimestamp - message.createdTimestamp}ms)`);
     });
 };
 
