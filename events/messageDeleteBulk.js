12bda0053642b63a37737992de2cceb3f6849d44
William K <random@williamk.ga>
2020-11-01 11:50:34 -0500

    Create messageDeleteBulk.js

---
+++ events/messageDeleteBulk.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = messages => {
+const message = messages.random(1);
+  if (!message.guild) return;
+  const logs = message.guild.channels.cache.find(channel => channel.name === "bot-logs");
+  if (message.guild.me.hasPermission("MANAGE_CHANNELS") && !logs) {
+    message.guild.channels.create("bot-logs", { type: "text" });
+  }
+
+const embed = new MessageEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL())
+    .setColor(0xEB5234)
+    .setTimestamp()
+    .setDescription(`**Bulk Delete in ${message.channel}, ${messages.size} messages deleted**`);
+    if (logs) logs.send({embed});
+  };
