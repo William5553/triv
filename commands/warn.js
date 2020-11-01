1b8e25e0f1835ac8691323a1a0ca0baa88055422
William K <random@williamk.ga>
2020-11-01 11:33:19 -0500

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,17 +1,17 @@
-const Discord = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 exports.run = (client, message, args) => {
   const reason = args.slice(1).join(' ');
-  const user = message.mentions.users.first();
+  const userr = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
   const modlog = message.guild.channels.cache.find(
       channel => channel.name === 'bot-logs'
     );
   if (!modlog) return message.reply('I cannot find a bot-logs channel');
-  if (reason.length < 1) return message.reply('You must supply a reason for the warning.');
-  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
-  const embed = new Discord.MessageEmbed()
+  if (reason.length < 1) return message.reply('supply a reason for the warning');
+  if (!userr) return message.reply('tell me who to warn idiot').catch(console.error);
+  const embed = new MessageEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
-    .setDescription(`**Action:** Warning\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}\n**User ID:** ${user.tag}`);
+    .setDescription(`**Action:** Warning\n**Target:** ${userr.user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}\n**User ID:** ${userr.user.id}`);
   return client.channels.fetch(modlog.id).send({embed});
 };
 
