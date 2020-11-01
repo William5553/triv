13ec0d7da5443d5f90640dda40268e47c8ac3e50
William K <random@williamk.ga>
2020-11-01 13:58:53 -0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -30,7 +30,7 @@ exports.run = async (client, message, args) => {
     .setDescription(`**Action:** Un/mute\n**Target:** ${userr.user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}\n**User ID:** ${userr.user.tag}`)
     .setFooter(`ID ${caseNum}`);
 
-  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply('I do not have the **MANAGE_ROLES_OR_PERMISSIONS** permission').catch(console.error);
+  if (!message.guild.me.hasPermission('MANAGE_ROLES')) return message.reply('I do not have the **MANAGE_ROLES** permission').catch(console.error);
 
   if (userr.roles.cache.has(muteRole.id)) {
     userr.roles.remove(muteRole.id, reason).then(() => {
