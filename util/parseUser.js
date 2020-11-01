7291b531599b299eeed778a2cd5c4542bb40ef40
William K <random@williamk.ga>
2020-11-01 12:56:42 -0500

    Update parseUser.js

--- util/parseUser.js
+++ util/parseUser.js
@@ -3,7 +3,7 @@ exports.parseUser = (message, user) => {
   if (user.id === message.author.id) {
     return message.reply('you can\'t do that to yourself, why did you try you idiot?');
   } else if (member) {
-    if (member.highestRole.position >= message.member.highestRole.position) return message.channel.send('That member is higher or equal to you');
+    if (member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send('That member is higher or equal to you');
   }
   return user;
 };
