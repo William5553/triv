9af475e1cc76fc6ea8fbbf9960019373d4d78f10
William K <random@williamk.ga>
2020-11-01 00:20:46 -0400

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,14 +1,14 @@
 const { MessageEmbed } = require("discord.js");
 const lyricsFinder = require("lyrics-finder");
 
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
 
     let lyrics = null;
 
     try {
-      lyrics = lyricsFinder(queue.songs[0].title, "");
+      lyrics = await lyricsFinder(queue.songs[0].title, "");
       if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
     } catch (error) {
       lyrics = `No lyrics found for ${queue.songs[0].title}.`;
@@ -23,8 +23,7 @@ exports.run = (client, message, args) => {
     if (lyricsEmbed.description.length >= 2048)
       lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
     return message.channel.send(lyricsEmbed).catch(console.error);
-};
-
+  };
 
   exports.conf = {
   enabled: true,
