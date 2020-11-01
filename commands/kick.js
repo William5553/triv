cfd3c8c940cb6081ed13e8a0adc45c6911f5506f
William K <random@williamk.ga>
2020-11-01 11:29:49 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,13 +3,13 @@ const {caseNumber} = require('../util/caseNumber.js');
 const {parseUser} = require('../util/parseUser.js');
 const settings = require('../settings.json');
 exports.run = async (client, message, args) => {
-  const user = message.mentions.users.first();
-  if (!user) return message.reply('tag someone to mute next time before I mute you');
-  parseUser(message, user);
-  if (user.id == client.user.id) {
+  const userr = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+  if (!userr) return message.reply('tag someone to kick next time before I kick you');
+  parseUser(message, userr.user);
+  if (userr.user.id == client.user.id) {
     return message.channel.send('You cannot, fool!');
   }
-  if (user.id == settings.ownerid) {
+  if (userr.user.id == settings.ownerid) {
     return message.reply('not willeh!');
   }
   const botlog = message.guild.channels.cache.find(
@@ -17,14 +17,13 @@ exports.run = async (client, message, args) => {
     );
   const caseNum = await caseNumber(client, botlog);
   if (!botlog) return message.channel.send('I cannot find a channel named bot-logs');
-  if (message.mentions.users.size < 1) return message.channel.send('You must mention someone to kick them.').catch(console.error);
-  message.guild.member(user).kick();
-  message.user.send(`Seems like you have been kicked from ${message.channel.guild.name}`);
+  userr.kick();
+  userr.user.send(`Seems like you have been kicked from ${message.channel.guild.name}`);
   const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
   const embed = new MessageEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
-    .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}\n**User ID:** ${user.id}`)
+    .setDescription(`**Action:** Kick\n**Target:** ${userr.user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}\n**User ID:** ${userr.user.id}`)
     .setFooter(`ID ${caseNum}`);
   return client.channels.fetch(botlog.id).send({embed});
 };
