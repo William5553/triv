7bc2710a2aa9afa925afa8af23c8cd888440acf5
William K <random@williamk.ga>
2020-11-01 15:11:45 -0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -20,7 +20,7 @@ exports.run = async (client, message, args) => {
     message.guild.channels.create('bot-logs', { type: 'text' });
   } else if (!botlog) 
   return message.reply('I cannot find a bot-logs channel').catch(console.error);
-  if (!muteRole) return message.reply('I cannot find a role named **Muted**').catch(console.error);
+  if (!muteRole) return message.reply('I cannot find a role named **muted**').catch(console.error);
   if (!userr) return message.reply('You must mention someone to mute them.').catch(console.error);
   const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
 
@@ -31,7 +31,7 @@ exports.run = async (client, message, args) => {
     .setFooter(`ID ${caseNum}`);
 
   if (!message.guild.me.hasPermission('MANAGE_ROLES')) return message.reply('I do not have the **MANAGE_ROLES** permission').catch(console.error);
-
+message.channel.updateOverwrite(muteRole, { SEND_MESSAGES: false })
   if (userr.roles.cache.has(muteRole.id)) {
     userr.roles.remove(muteRole.id, reason).then(() => {
       botlog.send({embed}).catch(console.error);
