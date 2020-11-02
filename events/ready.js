107be5482961b0ff3629aba7e43c4a07e2527d9e
William K <contact@williamk.ga>
2020-11-02 13:21:42 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
 const chalk = require('chalk');
+const settings = require('../settings.json');
 module.exports = client => {
   console.log(chalk.bgGreen.black('READY!'));
   if (client.guilds.cache.size >= 2) {
-    client.user.setActivity(`~help | ${client.guilds.cache.size} guilds`, { type: 'LISTENING'});
+    client.user.setActivity(`${settings.prefix}help | ${client.guilds.cache.size} guilds`, { type: 'LISTENING'});
   } else {
-    client.user.setActivity(`~help | ${client.guilds.cache.size} guild`, { type: 'LISTENING'});
+    client.user.setActivity(`${settings.prefix}help | ${client.guilds.cache.size} guild`, { type: 'LISTENING'});
   }
 };
