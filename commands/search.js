dfa76ff0862bd96fc5716074d9b46a0e10da6263
William K <contact@williamk.ga>
2020-11-02 12:13:43 -0500

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,49 +1,61 @@
 const { MessageEmbed } = require("discord.js");
-const settings = require ('../settings.json');
+const settings = require("../settings.json");
 const YouTubeAPI = require("simple-youtube-api");
-
 
 const youtube = new YouTubeAPI(settings.yt_api_key);
 
 exports.run = async (client, message, args) => {
-    if (!args.length)
-      return message.reply(`Usage: ${settings.prefix}search <Video Name>`).catch(console.error);
-    if (message.channel.activeCollector)
-      return message.reply("A message collector is already active in this channel.");
-    if (!message.member.voice.channel)
-      return message.reply("You need to join a voice channel first!").catch(console.error);
+  if (!args.length)
+    return message
+      .reply(`Usage: ${settings.prefix}search <Video Name>`)
+      .catch(console.error);
+  if (message.channel.activeCollector)
+    return message.reply(
+      "A message collector is already active in this channel."
+    );
+  if (!message.member.voice.channel)
+    return message
+      .reply("You need to join a voice channel first!")
+      .catch(console.error);
 
-    const search = args.join(" ");
+  const search = args.join(" "); 
+  const resultsEmbed = new MessageEmbed()
+    .setTitle(`**Reply with the song number you want to play**`)
+    .setDescription(`Results for: ${search}`)
+    .setColor("#F8AA2A");
 
-    let resultsEmbed = new MessageEmbed()
-      .setTitle(`**Reply with the song number you want to play**`)
-      .setDescription(`Results for: ${search}`)
-      .setColor("#F8AA2A");
+  try {
+    const results = await youtube.searchVideos(search, 10);
+    results.map((video, index) =>
+      resultsEmbed.addField(video.shortURL, `${index + 1}. ${video.title}`)
+    );
 
-    try {
-      const results = await youtube.searchVideos(search, 10);
-      results.map((video, index) => resultsEmbed.addField(video.shortURL, `${index + 1}. ${video.title}`));
+    var resultsMessage = await message.channel.send(resultsEmbed);
 
-      var resultsMessage = await message.channel.send(resultsEmbed);
-
-      function filter(msg) {
-        const pattern = /(^[1-9][0-9]{0,1}$)/g;
-        return pattern.test(msg.content) && parseInt(msg.content.match(pattern)[0]) <= 10;
-      }
-
-      message.channel.activeCollector = true;
-      const response = await message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] });
-      const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
-
-      message.channel.activeCollector = false;
-      message.client.commands.get("play").execute(message, [choice]);
-      resultsMessage.delete().catch(console.error);
-    } catch (error) {
-      console.error(error);
-      message.channel.activeCollector = false;
+    function filter(msg) {
+      const pattern = /(^[1-9][0-9]{0,1}$)/g;
+      return (
+        pattern.test(msg.content) &&
+        parseInt(msg.content.match(pattern)[0]) <= 10
+      );
     }
-  };
 
+    message.channel.activeCollector = true;
+    const response = await message.channel.awaitMessages(filter, {
+      max: 1,
+      time: 30000,
+      errors: ["time"]
+    });
+    const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
+
+    message.channel.activeCollector = false;
+    message.client.commands.get("play").run(message, [choice]);
+    resultsMessage.delete().catch(console.error);
+  } catch (error) {
+    console.error(error);
+    message.channel.activeCollector = false;
+  }
+};
 
 exports.conf = {
   enabled: true,
@@ -53,7 +65,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'search',
-  description: 'Search and select videos to listen to',
-  usage: 'search [video name]'
+  name: "search",
+  description: "Search and select videos to listen to",
+  usage: "search [video name]"
 };
