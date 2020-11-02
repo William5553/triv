abdf016b6d55aeb692263525962c64cb939d8518
William K <contact@williamk.ga>
2020-11-02 18:42:42 -0500

    Update fn.js

--- commands/fn.js
+++ commands/fn.js
@@ -1,11 +1,11 @@
-const fetch = require("node-fetch");
-const { MessageEmbed } = require("discord.js");
-const settings = require("../settings.json");
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+const settings = require('../settings.json');
 exports.run = async (c, m, a) => {
   if (a.length >= 2) {
     let platform = a[0].toLowerCase();
-    if (platform == "xbox" || platform == 'xb') platform = "xbl";
-    if (platform == "ps4" || platform == 'ps' || platform == 'playstation') platform = "psn";
+    if (platform == 'xbox' || platform == 'xb' || platform == 'xb1') platform = 'xbl';
+    if (platform == 'ps4' || platform == 'ps5' || platform == 'ps' || platform == 'playstation') platform = 'psn';
     if (!(platform == "pc" || platform == "psn" || platform == "xbl")) {
       return m.reply({
         embed: new MessageEmbed()
@@ -23,9 +23,9 @@ exports.run = async (c, m, a) => {
   });
     var e = await m.reply({
       embed: new MessageEmbed()
-        .setTitle("Working...")
+        .setTitle('Working...')
         .setDescription(`Please wait a few seconds`)
-        .setColor("#ffdd57")
+        .setColor('#ffdd57')
     });
     var r = await fetch(
       `https://api.fortnitetracker.com/v1/profile/${platform}/${epicName}`,
@@ -38,12 +38,12 @@ exports.run = async (c, m, a) => {
     var j = await r.json();
     if (j.error) {
       var text = j.error;
-      if (text == "Player Not Found") {
+      if (text == 'Player Not Found') {
         return e.edit({
           embed: new MessageEmbed()
             .setAuthor(
-              "404: Account not found.",
-              "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png"
+              '404: Account not found.',
+              'https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png'
             )
             .setColor("#ff3860")
             .setFooter(`Make sure you've got the name correct!`)
@@ -52,7 +52,7 @@ exports.run = async (c, m, a) => {
         return e.edit({
           embed: new MessageEmbed()
             .setAuthor(
-              "500: Something broke",
+              '500: Something broke',
               "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png"
             )
             .setColor("#ff3860")
@@ -62,9 +62,9 @@ exports.run = async (c, m, a) => {
     } else {
       var emb = new MessageEmbed()
         .setAuthor(`[${j.platformNameLong}] ${j.epicUserHandle}`)
-        .setColor("#23d160")
-        .setFooter("Epic Account ID: " + j.accountId)
-        .setThumbnail("https://i.imgur.com/QDzGMB8.png")
+        .setColor('#23d160')
+        .setFooter('Epic Account ID: ' + j.accountId)
+        .setThumbnail('https://i.imgur.com/QDzGMB8.png')
         .setURL(
           `https://fortnitetracker.com/profile/${j.platformName}/${j.epicUserHandle}`
         )
@@ -82,10 +82,10 @@ exports.run = async (c, m, a) => {
     return m.reply({
       embed: new MessageEmbed()
         .setAuthor(
-          "400: Too few arguments.",
-          "https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png"
+          '400: Too few arguments.',
+          'https://cdn.discordapp.com/attachments/423185454582464512/425761155940745239/emote.png'
         )
-        .setColor("#ff3860")
+        .setColor('#ff3860')
         .setDescription(
           `This command requires 2 arguments, **platform** and **epic username**. Try this **${settings.prefix}fn psn William5553YT**`
         )
@@ -101,7 +101,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: "fn",
-  description: "Gets a players fortnite stats",
-  usage: "fn [platform] [username]"
+  name: 'fn',
+  description: 'Gets a players fortnite stats',
+  usage: 'fn [platform] [username]'
 };
