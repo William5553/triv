a06ee4d1bb67e5d16f9f6912c84eea09bcbd56cf
William K <contact@williamk.ga>
2020-11-02 10:49:53 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,6 @@
 const settings = require('../settings.json');
 module.exports = message => {
-  const client = message.client;
-  if (message.author.id === client.user.id || message.author.bot || message.channel.type !== 'text') return;  
+  if (message.author.bot || message.channel.type !== 'text') return;  
   if (!message.content.startsWith(settings.prefix)) return;
   const command = message.content.split(' ')[0].slice(settings.prefix.length);
   const params = message.content.split(' ').slice(1);
