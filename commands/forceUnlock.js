b200945043dc5c1f5be344241f0a7ce5ed7cd383
William K <contact@williamk.ga>
2020-11-03 14:22:36 -0500

    Create forceUnlock.js

---
+++ commands/forceUnlock.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {
+const chan = message.guild.channels.cache.resolve(args[0]);
+if (!chan) return message.reply('please specify a valid channel id');
+chan.updateOverwrite(chan.guild.roles.everyone, { SEND_MESSAGES: null }).then(message.channel.send('Lockdown lifted.')).catch(console.error);
+delete client.lockit[chan.id];
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['fu'],
+  permLevel: 3
+};
+
+exports.help = {
+  name: 'forceunlock',
+  description: 'This will lock a channel down for the specified duration, disallowing members to speak.',
+  usage: 'forceunlock [channel id]'
+};
