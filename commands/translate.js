c9e1be67da74302cf3570d8c20b313424f1e48b1
William K <contact@williamk.ga>
2020-11-04 15:57:39 -0500

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -4,15 +4,14 @@ const translate = require('google-translate-api');
 exports.run = (client, message, args) => {
   if (args.length < 3) {
     return message.reply('Wrong format: An example would be `!translate swedish korean swedish-text-here` where after `!translate` would translate the `swedish-text-here` into korean');
-    const argFrom = args[0].toLowerCase();
-    const argTo = args[1].toLowerCase();
+    
     /*
     let lang_from = language.filter(ele => ele.name === argFrom)[0].abrv;
     let lang_to = language.filter(ele => ele.name === argTo)[0].abrv;
     */
     const text = args.slice(2).join(' ');
 
-    translate(text, { from: argFrom, to: argTo })
+    translate(text, { from: args[0], to: args[1] })
       .then(res => {
         if (res.from.text.autoCorrected === true) message.channel.send(`Autocorrected to: ${res.from.text.value}`);
         message.channel.send(res.text);
