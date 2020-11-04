8064a57fc231fa83a04755bafc0b66860656ecfa
William K <contact@williamk.ga>
2020-11-04 15:36:16 -0500

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,24 +1,27 @@
-const language = require('../util/langOptions');
-const translate = require('google-translate-api');
+const language = require("../util/langOptions");
+const translate = require("google-translate-api");
 
 exports.run = (client, message, args) => {
-if (args.length < 3) {
-            message.reply("Wrong format: An example would be `!translate swedish korean swedish-text-here` where after `!translate` would translate the `swedish-text-here` into korean");
-        } else {
-            let argFrom = args[0].toLowerCase();
-            let argTo = args[1].toLowerCase();
+  if (args.length < 3) {
+    message.reply(
+      "Wrong format: An example would be `!translate swedish korean swedish-text-here` where after `!translate` would translate the `swedish-text-here` into korean"
+    );
+  } else {
+    let argFrom = args[0].toLowerCase();
+    let argTo = args[1].toLowerCase();
+    /*
+    let lang_from = language.filter(ele => ele.name === argFrom)[0].abrv;
+    let lang_to = language.filter(ele => ele.name === argTo)[0].abrv;
+    */
+    let text = args.slice(2).join(" ");
 
-            let lang_from = language.filter(ele => ele.name === argFrom)[0].abrv;
-            let lang_to = language.filter(ele => ele.name === argTo)[0].abrv;
-            let text = args.slice(2).join(' ');
+    translate(text, { from: argFrom, to: argTo })
+      .then(res => message.channel.send(res.text))
+      .catch(err => message.channel.send(err));
+  }
+};
 
-            translate(text, {from: lang_from, to: lang_to})
-                .then(res => message.channel.send(res.text))
-                .catch(err => console.log(`Error: ${err}`));
-        }
-        };
-        
-        exports.conf = {
+exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
@@ -26,7 +29,7 @@ if (args.length < 3) {
 };
 
 exports.help = {
-  name: 'translate',
-  description: 'Translates text',
-  usage: 'translate [language from] [language to] [text]'
+  name: "translate",
+  description: "Translates text",
+  usage: "translate [language from] [language to] [text]"
 };
