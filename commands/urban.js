7f63286db79b5989bf3f5fb3ebb1e1a5f4bfa608
William K <contact@williamk.ga>
2020-11-04 11:12:28 -0500

    Update urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -1,20 +1,20 @@
-const {MessageEmbed} = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 const ud = require("relevant-urban");
 exports.run = async (client, message, args) => {
-  let worder = args[0];
-if(!worder) return message.channel.send("Specify a word")
-let defin = await ud(args.join(' ')).catch(e => {
-  message.channel.send("Word not found")
-  return;
-});
-let embed = new MessageEmbed()
-.setTitle(defin.word)
-.setURL(defin.urbanURL)
-.setDescription(defin.definition)
-.addField("Example", defin.example)
-.setFooter(`Author: ${defin.author}`)
-.setColor(0x0be05d)
-message.channel.send(embed)
+  const worder = args[0];
+  if (!worder) return message.channel.send("Specify a word");
+  const defin = await ud(args.join(" ")).catch(e => {
+    message.channel.send("Word not found");
+    return;
+  });
+  const embed = new MessageEmbed()
+    .setTitle(defin.word)
+    .setURL(defin.urbanURL)
+    .setDescription(defin.definition)
+    .addField("Example", defin.example)
+    .setFooter(`Author: ${defin.author}`)
+    .setColor(0x0be05d);
+  message.channel.send(embed);
 };
 
 exports.conf = {
