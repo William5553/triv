6169e5ba019d804580c7d7fc8a93dfc9b155f623
William K <contact@williamk.ga>
2020-11-04 16:01:24 -0500

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -2,21 +2,22 @@ const language = require('../util/langOptions');
 const translate = require('google-translate-api');
 
 exports.run = (client, message, args) => {
-  if (args.length < 3) {
+  if (args.length < 3)
     return message.reply('Wrong format: An example would be `!translate swedish korean swedish-text-here` where after `!translate` would translate the `swedish-text-here` into korean');
-    
-    /*
+
+  /*
     let lang_from = language.filter(ele => ele.name === argFrom)[0].abrv;
     let lang_to = language.filter(ele => ele.name === argTo)[0].abrv;
     */
-    const text = args.slice(2).join(' ');
+  const text = args.slice(2).join(' ');
 
-    translate(text, { from: args[0], to: args[1] })
-      .then(res => {
-        if (res.from.text.autoCorrected === true) message.channel.send(`Autocorrected to: ${res.from.text.value}`);
-        message.channel.send(res.text);
-      })
-      .catch(err => message.channel.send(err));
+  translate(text, { from: args[0], to: args[1] })
+    .then(res => {
+      if (res.from.text.autoCorrected === true)
+        message.channel.send(`Autocorrected to: ${res.from.text.value}`);
+      message.channel.send(res.text);
+    })
+    .catch(err => message.channel.send(err));
 };
 
 exports.conf = {
