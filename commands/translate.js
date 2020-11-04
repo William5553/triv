e5e007a0103d4302655bae8b7b81597f81b223e8
William K <contact@williamk.ga>
2020-11-04 18:57:11 -0500

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -6,8 +6,10 @@ exports.run = (client, message, args) => {
     return message.reply(`Wrong format: An example would be \`${settings.prefix}translate en fr english-text-here\` which would translate \`english-text-here\` into french`);
   const text = args.slice(2).join(' ');
 
-  const t = await translate(text, { from: args[0], to: args[1] }).catch(err => message.channel.send(err));
-  message.channel.send(t);
+  const t = await translate(text, { from: args[0], to: args[1] }).catch(err => {
+    if (err) return message.channel.send(err);
+  });
+  if (t) message.channel.send(t);
 };
 
 exports.conf = {
