830dd600e9edc658571170173903511dd1771e0f
William K <contact@williamk.ga>
2020-11-04 16:01:46 -0500

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -6,9 +6,9 @@ exports.run = (client, message, args) => {
     return message.reply('Wrong format: An example would be `!translate swedish korean swedish-text-here` where after `!translate` would translate the `swedish-text-here` into korean');
 
   /*
-    let lang_from = language.filter(ele => ele.name === argFrom)[0].abrv;
-    let lang_to = language.filter(ele => ele.name === argTo)[0].abrv;
-    */
+  let lang_from = language.filter(ele => ele.name === argFrom)[0].abrv;
+  let lang_to = language.filter(ele => ele.name === argTo)[0].abrv;
+  */
   const text = args.slice(2).join(' ');
 
   translate(text, { from: args[0], to: args[1] })
