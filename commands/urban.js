90fabbfedefaad675bb9690eae919f7b22645e23
William K <contact@williamk.ga>
2020-11-04 11:06:25 -0500

    Create urban.js

---
+++ commands/urban.js
@@ -0,0 +1,31 @@
+const {MessageEmbed} = require('discord.js');
+const ud = require("relevant-urban");
+exports.run = async (client, message, args) => {
+  let worder = args[0];
+if(!worder) return message.channel.send("Specify a word")
+let defin = await ud(args.join(' ')).catch(e => {
+  message.channel.send("Word not found")
+  return;
+});
+let embed = new MessageEmbed()
+.setTitle(defin.word)
+.setURL(defin.urbanURL)
+.setDescription(defin.definition)
+.addField("Example", defin.example)
+.setFooter(`Author: ${defin.author}`)
+.setColor(config.color)
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['ud'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'urban',
+  description: 'Searches for a term on the urban dictionary',
+  usage: 'urban [term]'
+};
