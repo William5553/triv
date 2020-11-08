3ad4a51c4cc3c1b7b579cbf84ab2ec2bc9828645
William K <contact@williamk.ga>
2020-11-08 17:52:37 -0500

    Update messageDeleteBulk.js

--- events/messageDeleteBulk.js
+++ events/messageDeleteBulk.js
@@ -5,9 +5,6 @@ const length = messages.array().length;
   const channel = messages.first().channel.name;
   if (!message.guild) return;
   const logs = message.guild.channels.cache.find(channel => channel.name === "bot-logs");
-  if (message.guild.me.hasPermission("MANAGE_CHANNELS") && !logs) {
-    message.guild.channels.create("bot-logs", { type: "text" });
-  }
 
 const embed = new MessageEmbed()
     .setAuthor(message.guild.name, message.guild.iconURL())
