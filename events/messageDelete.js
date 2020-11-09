4366115c45a58e272e15961d06a41c001ffad521
William K <contact@williamk.ga>
2020-11-09 15:31:56 -0500

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -2,9 +2,8 @@ const { MessageEmbed } = require("discord.js");
 module.exports = message => {
   if (!message.guild) return;
   const logs = message.guild.channels.cache.find(channel => channel.name === "bot-logs");
-  if (message.guild.me.hasPermission("MANAGE_CHANNELS") && !logs) {
+  if (message.guild.me.hasPermission("MANAGE_CHANNELS") && !logs)
     message.guild.channels.create("bot-logs", { type: "text" });
-  }
 
   const embed = new MessageEmbed()
     .setTitle("**Message Deleted**")
@@ -12,5 +11,8 @@ module.exports = message => {
     .setTimestamp()
     .setDescription(message.content)
     .setColor("0xEB5234");
-  if (logs) logs.send(embed);
+  if (logs) {
+    logs.send(embed);
+    logs.updateOverwrite(message.channel.guild.roles.everyone, { SEND_MESSAGES: false });
+  }
 };
