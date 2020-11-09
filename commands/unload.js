5c4934875b81054c20922cd190fd2c6acc50cabe
William K <contact@williamk.ga>
2020-11-09 12:25:55 -0500

    Update unload.js

--- commands/unload.js
+++ commands/unload.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args) => {
   if (!args[0]) return message.reply('specify a command to unload boomer');
   const res = client.unloadCommand(args[0]);
-  if (!res) message.channel.send(`Unloaded ${args[0]}`);
+  if (res === false) return message.channel.send(`Unloaded ${args[0]}`);
   else message.channel.send(res);
 };
 
