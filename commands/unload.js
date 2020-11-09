405a861936a5b3edb03c22d0f5da7b0c3e0f66ec
William K <contact@williamk.ga>
2020-11-09 12:23:34 -0500

    Create unload.js

---
+++ commands/unload.js
@@ -0,0 +1,19 @@
+exports.run = async (client, message, args) => {
+  if (!args[0]) return message.reply('specify a command to unload boomer');
+  const res = client.unloadCommand(args[0]);
+  if (!res) message.channel.send(`Unloaded ${args[0]}`);
+  else message.channel.send(res);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'unload',
+  description: 'Unloads a command',
+  usage: 'unload [command]'
+};
