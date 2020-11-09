04c7893f5c9b8c23daeffb5a7ae7181deef5419e
William K <contact@williamk.ga>
2020-11-09 15:12:02 -0500

    Update unload.js

--- commands/unload.js
+++ commands/unload.js
@@ -1,10 +1,9 @@
 exports.run = async (client, message, args) => {
   let command;
-  if (client.commands.has(args[0])) {
+  if (client.commands.has(args[0]))
     command = args[0];
-  } else if (client.aliases.has(args[0])) {
+  else if (client.aliases.has(args[0])) {
     command = client.aliases.get(args[0]);
-  }
   if (!command) 
     return message.channel.send(`I cannot find the command: ${args[0]}`);
   else 
