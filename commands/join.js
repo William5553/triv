22d716c34c65d5e6ef92c25034f4c760907cb839
William K <contact@williamk.ga>
2020-11-20 10:48:58 -0500

    Create join.js

---
+++ commands/join.js
@@ -0,0 +1,27 @@
+exports.run = (client, message) => {
+  const { channel } = message.member.voice;
+  
+  const permissions = channel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT"))
+      return message.reply("Cannot connect to voice channel, missing the **CONNECT** permission");
+    if (!permissions.has("SPEAK"))
+      return message.reply("I cannot speak in this voice channel, make sure I have the **SPEAK** permission!");
+  
+  if (channel)
+    channel.join();
+  else
+    return message.reply('you have to be in a voice channel moron');
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'join',
+  description: 'Joins the voice channel',
+  usage: 'join'
+};
