f20174dea6fccc8b0897d895dc7d3122ac48ef88
William K <contact@williamk.ga>
2020-11-20 11:19:10 -0500

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,13 +1,12 @@
 exports.run = (client, message) => {
   const channell = message.member.voice.channel;
   
-  const permissions = channell.permissionsFor(message.client.user);
+  if (channell) {
+    const permissions = channell.permissionsFor(message.client.user);
     if (!permissions.has("CONNECT"))
       return message.reply("Cannot connect to voice channel, missing the **CONNECT** permission");
     if (!permissions.has("SPEAK"))
       return message.reply("I cannot speak in this voice channel, make sure I have the **SPEAK** permission!");
-  
-  if (channell) {
     channell.join()
       .then(connection => {
         connection.voice.setSelfDeaf(true);
