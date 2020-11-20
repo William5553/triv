4e46b8f02b4eb4e547b2998150a6b1196af7b77e
William K <contact@williamk.ga>
2020-11-20 11:17:40 -0500

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -11,7 +11,7 @@ exports.run = (client, message) => {
     channell.join()
       .then(connection => {
         connection.voice.setSelfDeaf(true);
-        message.channel.send('I\'ve joined the vc');
+        message.reply('I\'ve joined the vc');
       })
       .catch(message.channel.send);
   } else
