33843f7737e8d4aea50761033ecb946b70315097
William K <contact@williamk.ga>
2020-11-22 18:50:07 -0500

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -8,7 +8,7 @@ module.exports = (oldMessage, newMessage) => {
 
   const embed = new MessageEmbed()
     .setTitle("Message Edited")
-    .setAuthor(`${newMessage.author.tag} - #${newMessage.channel.name}`, newMessage.author.avatarURL())
+    .setAuthor(`${newMessage.author} - ${newMessage.channel}`, newMessage.author.avatarURL())
     .setTimestamp()
     .addField('**Old Message**', oldMessage.content, true)
     .addField('**New Message**', newMessage.content, true)
