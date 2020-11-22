9bbec4a96b420925407f229f140386bd9c80335b
William K <contact@williamk.ga>
2020-11-22 18:52:34 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,12 +3,13 @@ module.exports = message => {
   if (message.author.bot || !message.guild || !message.content.startsWith(settings.prefix)) return;
   const command = message.content.split(' ')[0].slice(settings.prefix.length);
   const params = message.content.split(' ').slice(1);
-  const perms = message.client.elevation(message);
+  const { client } = message;
+  const perms = client.elevation(message);
   let cmd;
-  if (message.client.commands.has(command)) {
-    cmd = message.client.commands.get(command);
-  } else if (message.client.aliases.has(command)) {
-    cmd = message.client.commands.get(message.client.aliases.get(command));
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
   }
   if (cmd) {
     if (perms < cmd.conf.permLevel) return;
