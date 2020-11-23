9e03e3285988861aaa12c460ad456335c8fbf154
William K <contact@williamk.ga>
2020-11-23 10:43:55 -0500

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -5,8 +5,7 @@ exports.run = (client, message, args) => {
     if (!queue) return message.reply('there ain\'t a queue').catch(client.logger.error);
     if (!canModifyQueue(message.member)) return;
     
-    if (!args.length) return message.reply(`Usage: ${settings.prefix}remove <Queue Number>`);
-    if (isNaN(args[0])) return message.reply(`Usage: ${settings.prefix}remove <Queue Number>`);
+    if (!args.length || isNaN(args[0])) return message.reply(`Usage: ${settings.prefix}remove <Queue Number>`);
 
     const song = queue.songs.splice(args[0] - 1, 1);
     queue.textChannel.send(`${message.author} ❌ removed **${song[0].title}** from the queue.`);
