9b42c017c656decd1de87b7231ca69340942b733
William K <contact@williamk.ga>
2020-11-22 19:01:48 -0500

    Update disconnect.js

--- events/disconnect.js
+++ events/disconnect.js
@@ -1,3 +1,3 @@
 module.exports = client => { //eslint-disable-line no-unused-vars
-  console.log(`You have been disconnected at ${new Date()}`);
+  client.logger.log(`You have been disconnected at ${new Date()}`);
 };
