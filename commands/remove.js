e3ba52914ef9d2f26dbb583b8dfa2d8ba0be9356
William K <contact@williamk.ga>
2020-11-22 19:14:15 -0500

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,16 +1,15 @@
 const { canModifyQueue } = require("../util/queue");
-
+const settings = require('../settings.json');
 exports.run = (client, message, args) => {
-    const queue = message.client.queue.get(message.guild.id);
+    const queue = client.queue.get(message.guild.id);
     if (!queue) return message.channel.send("There is no queue.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
     
-    if (!args.length) return message.reply(`Usage: ${message.client.prefix}remove <Queue Number>`);
-    if (isNaN(args[0])) return message.reply(`Usage: ${message.client.prefix}remove <Queue Number>`);
+    if (!args.length) return message.reply(`Usage: ${settings.prefix}remove <Queue Number>`);
+    if (isNaN(args[0])) return message.reply(`Usage: ${settings.prefix}remove <Queue Number>`);
 
     const song = queue.songs.splice(args[0] - 1, 1);
     queue.textChannel.send(`${message.author} ❌ removed **${song[0].title}** from the queue.`);
-
 };
 
 exports.conf = {
