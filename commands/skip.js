020b8723767dc8ac66bd2a98f1b52d4d5eb7c6f8
William K <contact@williamk.ga>
2020-11-22 19:26:26 -0500

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -3,12 +3,12 @@ const { canModifyQueue } = require("../util/queue");
 exports.run = (client, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue)
-      return message.reply("There is nothing playing that I could skip for you.").catch(console.error);
+      return message.reply("There is nothing playing that I could skip for you.").catch(client.logger.error);
     if (!canModifyQueue(message.member)) return;
 
     queue.playing = true;
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏭ skipped the song`).catch(console.error);
+    queue.textChannel.send(`${message.author} ⏭ skipped the song`).catch(client.logger.error);
 };
 
 exports.conf = {
