c16c52f488aa751c6c69e19fc740ab9bf3b6b75a
Glitch (william5553-discord-bot-4) <none>
2020-11-23 04:25:07 +0000

    🚵🎑 Updated with Glitch

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,36 +1,37 @@
-const settings = require('../settings.json');
 const { MessageEmbed } = require("discord.js");
-const Genius = require("genius-lyrics");
-const GClient = Genius.Client(settings.genius_api_key);
+const lyric = require("@allvaa/get-lyrics");
 
 exports.run = async (client, message, args) => {
-    const queue = client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is nothing playing.").catch(client.logger.error);
+  const queue = client.queue.get(message.guild.id);
+  if (!queue)
+    return message.channel
+      .send("There is nothing playing.")
+      .catch(client.logger.error);
 
-    let lyrics = null;
-    
-    const songtitle = queue.songs[0].title
-    .replace(/(official video/i, '')
-    .replace(/(official music video)/i, '');
+  let lyrics = null;
+  
+  const songtitle = queue.songs[0].title
+    .replace('/(official audio)/i', '')
+    .replace('/(dir*)/i', '')
+    .replace("/(official video)/i", "")
+    .replace("/(official music video*)/i", "");
 
-    try {
-      const searches = await GClient.songs.search(songtitle);
-      const firstSong = searches[0];
-      lyrics = await firstSong.lyrics();
-      if (!lyrics) lyrics = `No lyrics found for ${songtitle}.`;
-    } catch (error) {
-      lyrics = `No lyrics found for ${songtitle}.`;
-    }
+  try {
+    lyrics = await lyric(songtitle);
+    if (!lyrics) lyrics = `No lyrics found for ${songtitle}.`;
+  } catch (error) {
+    lyrics = `No lyrics found for ${songtitle}.`;
+  }
 
-    let lyricsEmbed = new MessageEmbed()
-      .setTitle("Lyrics")
-      .setDescription(lyrics)
-      .setColor("#F8AA2A");
+  const lyricsEmbed = new MessageEmbed()
+    .setTitle('Lyrics - ' + songtitle)
+    .setDescription(lyrics)
+    .setColor("#F8AA2A");
 
-    if (lyricsEmbed.description.length >= 2048)
-      lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
-    return message.channel.send(lyricsEmbed).catch(client.logger.error);
-  };
+  if (lyricsEmbed.description.length >= 2048)
+    lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+  return message.channel.send(lyricsEmbed).catch(client.logger.error);
+};
 
   exports.conf = {
   enabled: true,
