70d33b14360d4cd77c026009a8c74cbc9cf01e58
William K <contact@williamk.ga>
2020-11-22 23:56:45 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,20 +3,15 @@ exports.run = (client, message, args) => {
   if (!args[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    let yes = client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n');
-    let yes2 = null;
-    if (yes.length >= 1920) {
-      yes = `${yes.substr(0, 1920)}`;
-      yes2 = `${yes.substr(1921)}`;
+    let yes = `= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n ${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`;
+    for (i = 0; i*2000 < yes.length; i++) { 
+      yes = `${yes.substr(i*2000, i*2000+2000)}`;
+      message.author.send(yes, {code: 'asciidoc'}).catch(err => {
+        client.logger.error(err);
+        message.author.send(err);
+      });
     }
     message.channel.send('Help sent to your DMs! :mailbox_with_mail:');
-    message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${yes}`, {code: 'asciidoc'}).catch(err => {
-      client.logger.error(err);
-      message.author.send(err);
-    });
-    if (yes2) {
-      message.author.send(yes2, {code: 'asciidoc'}).catch(client.logger.error);
-    }
   } else {
     let command = args[0];
     if (client.commands.has(command)) {
