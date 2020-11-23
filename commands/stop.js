cfccba11c4e9807af50742f3d50fa236508ff647
William K <contact@williamk.ga>
2020-11-23 10:45:38 -0500

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -4,7 +4,6 @@ exports.run = (client, message, args) => {
     if (!message.guild.voice.channel) return message.reply('i\'m not in a voice channel moron');
     const queue = client.queue.get(message.guild.id);
     
-   // if (!queue) return message.reply("There is nothing playing.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
     if (queue) {
       queue.songs = [];
