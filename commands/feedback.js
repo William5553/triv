e3587c75f192d489eb0a814d9ed8ea25e786a371
William K <contact@williamk.ga>
2020-11-23 10:42:22 -0500

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -2,13 +2,13 @@ const {MessageEmbed} = require('discord.js');
 exports.run = (client, message, args) => {
   const feedbackid = '340924459026219009';
   const feedback = args.slice(0).join(' ');
-  if (feedback.length < 1) return message.reply('we don\'t accept blank feedback!').catch(console.error);
+  if (feedback.length < 1) return message.reply('we don\'t accept blank feedback!').catch(client.logger.error);
   message.reply('Feedback sent.. :envelope:');
   const embed = new MessageEmbed()
       .setColor(0x00AE86)
       .setTimestamp()
       .setDescription(`**Sent in by:** ${message.author.tag}\n\n**ID: ** ${message.author.id}\n\n**Feedback:** ` + feedback);
-  return client.channels.cache.get(feedbackid).send({embed});
+  return client.channels.cache.get(feedbackid).send({embed}).catch(client.logger.error);
 };
 
 exports.conf = {
