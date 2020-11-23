89be07f1a233eb3f891a4d961f6e55b2a9cd5bf7
William K <contact@williamk.ga>
2020-11-22 19:34:34 -0500

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -1,7 +1,7 @@
 const { canModifyQueue } = require("../util/queue");
 
 exports.run = (client, message, args) => {
-    const queue = message.client.queue.get(message.guild.id);
+    const queue = client.queue.get(message.guild.id);
     if (!queue)
         return message.reply("There is nothing playing.").catch(client.logger.error);
     if (!canModifyQueue(message.member))
