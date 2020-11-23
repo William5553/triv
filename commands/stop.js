d9b34c114514c8b373f55381a22d4083a64a74ef
William K <contact@williamk.ga>
2020-11-22 19:27:32 -0500

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -2,14 +2,14 @@ const { canModifyQueue } = require("../util/queue");
 
 
 exports.run = (client, message, args) => {
-    const queue = message.client.queue.get(message.guild.id);
+    const queue = client.queue.get(message.guild.id);
     
    // if (!queue) return message.reply("There is nothing playing.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
     if (queue) {
       queue.songs = [];
       queue.connection.dispatcher.end();
-      queue.textChannel.send(`${message.author} ⏹ stopped the music!`).catch(console.error);
+      queue.textChannel.send(`${message.author} ⏹ stopped the music!`).catch(client.logger.error);
     }
     else
       message.member.voice.channel.leave();
