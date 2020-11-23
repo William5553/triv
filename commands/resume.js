525ef543d4772bab1b797df41caf1be772cd0b16
William K <contact@williamk.ga>
2020-11-22 19:12:13 -0500

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -2,16 +2,16 @@ const { canModifyQueue } = require("../util/queue");
 
 exports.run = (client, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("There is nothing playing.").catch(client.logger.error);
     if (!canModifyQueue(message.member)) return;
 
     if (!queue.playing) {
       queue.playing = true;
       queue.connection.dispatcher.resume();
-      return queue.textChannel.send(`${message.author} ▶ resumed the music!`).catch(console.error);
+      return queue.textChannel.send(`${message.author} ▶ resumed the music!`).catch(client.logger.error);
     }
 
-    return message.reply("The queue is not paused.").catch(console.error);
+    return message.reply("The queue is not paused.").catch(client.logger.error);
 };
 
 exports.conf = {
