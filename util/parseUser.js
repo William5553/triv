6d4b1de726de17cabb7d63844555fbce3013eec1
William K <contact@williamk.ga>
2020-11-24 12:44:46 -0500

    Update parseUser.js

--- util/parseUser.js
+++ util/parseUser.js
@@ -1,5 +1,5 @@
 exports.parseUser = (message, user) => {
-  const member = message.guild.members.resolve(user) || null;
+  const member = message.guild.members.fetch(user) || null;
   if (user.id === message.author.id) {
     return message.reply('you can\'t do that to yourself, why did you try, you idiot');
   } else if (member) {
