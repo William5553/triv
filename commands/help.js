16e4dcc8a816b8de9c2a44784706a888fa4ff451
William K <contact@williamk.ga>
2020-11-25 15:12:06 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,7 @@
 const settings = require('../settings.json');
 exports.run = (client, message, args, perms) => {
   if (!args[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    const longest = Array.from(client.commands.keys()).reduce((long, str) => Math.max(long, str.length), 0);
     const fonk = client.commands
       .map(c => {
         if (perms < c.conf.permLevel)
@@ -11,7 +10,7 @@ exports.run = (client, message, args, perms) => {
           return `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`
       })
       .filter(a => a !== null)
-      .join('\n')
+      .join('\n');
     const msg = `= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${fonk}`;
     let i;
     for (i = 0; i * 1980 <= msg.length; i++) {
@@ -32,7 +31,7 @@ exports.run = (client, message, args, perms) => {
     if (!command) return message.channel.send(`${args[0]} is not a valid command`);
     const aliases = command.conf.aliases.join(', ') || 'none';
     message.channel.send(
-      `= ${command.help.name} = \n${command.help.description}\nusage :: ${command.help.usage}\naliases :: ${aliases}`,
+      `= ${command.help.name} = \n${command.help.description}\nusage   :: ${command.help.usage}\naliases :: ${aliases}`,
       {
         code: 'asciidoc',
       }
