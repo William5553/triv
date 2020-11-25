49692c12444c0ced0b71b10ad63f33867ace599d
William K <contact@williamk.ga>
2020-11-25 15:56:22 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,49 +3,7 @@ const { MessageEmbed } = require('discord.js');
 const settings = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
 module.exports = message => {
   const { client } = message;
-  if (message.author.bot) return;
-  if (message.guild) {
-    const xp = require('../xp.json');
-    const xpAdd = Math.floor(Math.random() * 7) + 8;
-
-    if (!xp[message.guild.id]) {
-      xp[message.guild.id] = {
-        '-1': {
-          level: -1,
-          xp: -1,
-          messagessent: -1,
-        },
-      };
-    }
-    if (!xp[message.guild.id][message.author.id]) {
-      xp[message.guild.id][message.author.id] = {
-        level: 1,
-        xp: 0,
-        messagessent: 0,
-      };
-    }
-
-    const messagessent = xp[message.guild.id][message.author.id].messagessent;
-    const curxp = xp[message.guild.id][message.author.id].xp;
-    const curlvl = xp[message.guild.id][message.author.id].level;
-    const nxtLvl = xp[message.guild.id][message.author.id].level * 200;
-    xp[message.guild.id][message.author.id].xp = curxp + xpAdd;
-    xp[message.guild.id][message.author.id].messagessent = messagessent + Number(1);
-    if (nxtLvl <= xp[message.guild.id][message.author.id].xp) {
-      xp[message.guild.id][message.author.id].level = curlvl + 1;
-      const lvlup = new MessageEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL())
-        .setTitle('Level Up!')
-        .setColor(0x902b93)
-        .addField('New Level', curlvl + 1);
-
-      message.channel.send(lvlup);
-    }
-    fs.writeFile('../xp.json', JSON.stringify(xp), err => {
-      if (err) client.logger.error(err);
-    });
-  }
-  if (!message.content.startsWith(settings.prefix)) return;
+  if (message.author.bot || !message.content.startsWith(settings.prefix)) return;
   const command = message.content.split(' ')[0].slice(settings.prefix.length).toLowerCase();
   const params = message.content.split(' ').slice(1);
   let cmd;
