900f7aeeed54493c5fb54efd5a9e0fe2b4970706
Glitch (triv) <none>
2020-11-25 21:00:30 +0000

    🍲🛬 Updated with Glitch

--- index.js
+++ index.js
@@ -24,46 +24,46 @@ client.on('error', client.logger.error);
 client.on('message', async message => {
   if (!message.guild || message.author.bot) return;
   
-const xp = require('../xp.json');
-    const xpAdd = Math.floor(Math.random() * 7) + 8;
+  const xp = require('../xp.json');
+  const xpAdd = Math.floor(Math.random() * 7) + 8;
 
-    if (!xp[message.guild.id]) {
-      xp[message.guild.id] = {
-        '-1': {
-          level: -1,
-          xp: -1,
-          messagessent: -1,
-        },
-      };
-    }
+  if (!xp[message.guild.id]) {
+    xp[message.guild.id] = {
+      '-1': {
+        level: -1,
+        xp: -1,
+        messagessent: -1,
+      },
+    };
+  }
   
-    if (!xp[message.guild.id][message.author.id]) {
-      xp[message.guild.id][message.author.id] = {
-        level: 1,
-        xp: 0,
-        messagessent: 0,
-      };
-    }
+  if (!xp[message.guild.id][message.author.id]) {
+    xp[message.guild.id][message.author.id] = {
+      level: 1,
+      xp: 0,
+      messagessent: 0,
+    };
+  }
 
-    const messagessent = xp[message.guild.id][message.author.id].messagessent;
-    const curxp = xp[message.guild.id][message.author.id].xp;
-    const curlvl = xp[message.guild.id][message.author.id].level;
-    const nxtLvl = xp[message.guild.id][message.author.id].level * 200;
-    xp[message.guild.id][message.author.id].xp = curxp + xpAdd;
-    xp[message.guild.id][message.author.id].messagessent = messagessent + Number(1);
-    if (nxtLvl <= xp[message.guild.id][message.author.id].xp) {
-      xp[message.guild.id][message.author.id].level = curlvl + 1;
-      const lvlup = new MessageEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL())
-        .setTitle('Level Up!')
-        .setColor(0x902b93)
-        .addField('New Level', curlvl + 1);
+  const messagessent = xp[message.guild.id][message.author.id].messagessent;
+  const curxp = xp[message.guild.id][message.author.id].xp;
+  const curlvl = xp[message.guild.id][message.author.id].level;
+  const nxtLvl = xp[message.guild.id][message.author.id].level * 200;
+  xp[message.guild.id][message.author.id].xp = curxp + xpAdd;
+  xp[message.guild.id][message.author.id].messagessent = messagessent + Number(1);
+  if (nxtLvl <= xp[message.guild.id][message.author.id].xp) {
+    xp[message.guild.id][message.author.id].level = curlvl + 1;
+    const lvlup = new MessageEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL())
+      .setTitle('Level Up!')
+      .setColor(0x902b93)
+      .addField('New Level', curlvl + 1);
 
-      message.channel.send(lvlup);
-    }
-    fs.writeFile('../xp.json', JSON.stringify(xp), err => {
-      if (err) client.logger.error(err);
-    });
-})
+    message.channel.send(lvlup);
+  }
+  fs.writeFile('../xp.json', JSON.stringify(xp), err => {
+    if (err) client.logger.error(err);
+  });
+});
 
 client.login(settings.token);
