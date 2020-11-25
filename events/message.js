c63f748f40c9025d54210172dea34d1d13abb0f9
William K <contact@williamk.ga>
2020-11-25 13:10:15 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,16 +1,20 @@
 const settings = require('../settings.json');
 module.exports = message => {
   const { client } = message;
-  if (message.author.bot || !message.guild || !message.content.startsWith(settings.prefix)) return;
+  if (message.author.bot || !message.content.startsWith(settings.prefix)) return;
   const command = message.content.split(' ')[0].slice(settings.prefix.length).toLowerCase();
   const params = message.content.split(' ').slice(1);
-  const perms = client.elevation(message);
   let cmd;
   if (client.commands.has(command)) {
     cmd = client.commands.get(command);
   } else if (client.aliases.has(command)) {
     cmd = client.commands.get(client.aliases.get(command));
   }
+  if (message.channel.type === 'dm' && cmd.conf.name === 'help') {
+    cmd.run(client, message, params);
+  }
+  if (!message.guild) return;
+  const perms = client.elevation(message);
   if (cmd) {
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
