3fb38fc490c2ebcbe09b54c7dcd31c3e69e9b7d0
William K <contact@williamk.ga>
2020-11-25 15:57:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Collection } = require('discord.js');
+const { Client, Collection, MessageEmbed } = require('discord.js');
 const client = new Client({ disableMentions: 'everyone' });
 const fs = require('fs');
 const settings = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
@@ -21,5 +21,46 @@ fs.readdir('./commands/', (err, files) => {
 
 client.on('warn', client.logger.warn);
 client.on('error', client.logger.error);
+client.on('message', message => {
+const xp = require('../xp.json');
+    const xpAdd = Math.floor(Math.random() * 7) + 8;
+
+    if (!xp[message.guild.id]) {
+      xp[message.guild.id] = {
+        '-1': {
+          level: -1,
+          xp: -1,
+          messagessent: -1,
+        },
+      };
+    }
+    if (!xp[message.guild.id][message.author.id]) {
+      xp[message.guild.id][message.author.id] = {
+        level: 1,
+        xp: 0,
+        messagessent: 0,
+      };
+    }
+
+    const messagessent = xp[message.guild.id][message.author.id].messagessent;
+    const curxp = xp[message.guild.id][message.author.id].xp;
+    const curlvl = xp[message.guild.id][message.author.id].level;
+    const nxtLvl = xp[message.guild.id][message.author.id].level * 200;
+    xp[message.guild.id][message.author.id].xp = curxp + xpAdd;
+    xp[message.guild.id][message.author.id].messagessent = messagessent + Number(1);
+    if (nxtLvl <= xp[message.guild.id][message.author.id].xp) {
+      xp[message.guild.id][message.author.id].level = curlvl + 1;
+      const lvlup = new MessageEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL())
+        .setTitle('Level Up!')
+        .setColor(0x902b93)
+        .addField('New Level', curlvl + 1);
+
+      message.channel.send(lvlup);
+    }
+    fs.writeFile('../xp.json', JSON.stringify(xp), err => {
+      if (err) client.logger.error(err);
+    });
+})
 
 client.login(settings.token);
