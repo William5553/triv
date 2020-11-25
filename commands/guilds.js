0775683eb61af120434fc81d3483c802fc5cb6cf
William K <contact@williamk.ga>
2020-11-25 13:15:45 -0500

    Update guilds.js

--- commands/guilds.js
+++ commands/guilds.js
@@ -1,7 +1,5 @@
 const { MessageEmbed } = require('discord.js');
 exports.run = (client, message) => {
-  const settings = require('../settings.json');
-  if (message.author.id !== settings.ownerid) return message.reply("you're not willeh!");
   const embed = new MessageEmbed()
     .setColor(0x00ff5c)
     .setAuthor(client.user.username, client.user.avatarURL())
@@ -15,11 +13,11 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: 4,
+  permLevel: 4
 };
 
 exports.help = {
   name: 'guilds',
   description: 'Shows all my guilds.',
-  usage: 'guilds',
+  usage: 'guilds'
 };
