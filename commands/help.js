699d23519748185f854bfd449d8f23b6699f2b84
William K <random@williamk.ga>
2020-11-25 18:29:41 -0500

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -30,7 +30,7 @@ exports.run = (client, message, args, perms) => {
     }
     if (!command) return message.channel.send(`${args[0]} is not a valid command`);
     const aliases = command.conf.aliases.join(', ') || 'none';
-	const example = command.help.example || 'to be added';
+    const example = command.help.example || 'to be added';
     message.channel.send(
       `= ${command.help.name} = \n${command.help.description}\nusage    :: ${command.help.usage}\naliases  :: ${aliases}\nexample :: ${example}`,
       {
