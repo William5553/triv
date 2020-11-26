cbcb4cad60d14fec460c0e78f1fd09498283de88
William K <contact@williamk.ga>
2020-11-26 13:08:49 -0500

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -3,6 +3,7 @@ module.exports = client => {
   client.on('ready', () => reqEvent('ready')(client));
   client.on('reconnecting', () => reqEvent('reconnecting')(client));
   client.on('disconnect', () => reqEvent('disconnect')(client));
+  client.on('guildDelete', reqEvent('guildDelete'));
   client.on('message', reqEvent('message'));
   client.on('messageDelete', reqEvent('messageDelete'));
   client.on('messageDeleteBulk', reqEvent('messageDeleteBulk'));
