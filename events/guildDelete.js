de6f78f94152430f3e33405c673181f82dbb0672
William K <contact@williamk.ga>
2020-11-26 13:34:00 -0500

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 module.exports = guild => {
+  if (!guild.available) return guild.client.logger.error('guild is not available');
   const xp = JSON.parse(require('../xp.json'));
   
   if (xp[guild.id]) {
