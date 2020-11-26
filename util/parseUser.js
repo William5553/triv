37e8afe20b854f48a33f1505c2fa042c047d176b
William K <contact@williamk.ga>
2020-11-26 13:47:04 -0500

    Update parseUser.js

--- util/parseUser.js
+++ util/parseUser.js
@@ -1,10 +1,9 @@
-exports.parseUser = (message, user) => {
-  const member = message.guild.members.fetch(user) || null;
+exports.parseUser = (message, member) => {
   if (user.id === message.author.id) {
     return message.reply("you can't do that to yourself, why did you try, you idiot");
   } else if (member && member.roles) {
     if (member.roles.highest.position >= message.member.roles.highest.position)
       return message.channel.send('That member is higher or equal to you');
   }
-  return user;
+  return true;
 };
