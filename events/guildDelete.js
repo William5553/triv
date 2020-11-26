b4551426dba612bb73b95c6b2a372f375f2420f3
William K <contact@williamk.ga>
2020-11-26 13:17:35 -0500

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,9 +3,7 @@ module.exports = guild => {
   const xp = JSON.parse(require('../xp.json'));
   
   if (xp[guild.id]) {
-    xp[guild.id] = {
-    
-    }
+    delete xp[guild.id]
   }
   
   fs.writeFile('./xp.json', JSON.stringify(xp), err => {
