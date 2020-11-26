ed1baf1a1f60181c8a9576684df5735aa7a15687
William K <contact@williamk.ga>
2020-11-26 13:15:51 -0500

    Create guildDelete.js

---
+++ events/guildDelete.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+module.exports = guild => {
+  const xp = JSON.parse(require('../xp.json'));
+  
+  if (xp[guild.id]) {
+    xp[guild.id] = {
+    
+    }
+  }
+  
+  fs.writeFile('./xp.json', JSON.stringify(xp), err => {
+    if (err) guild.client.logger.error(err);
+  });
+};
