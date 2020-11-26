1e955efa6003cf2b0683454ce440c545f8a4fb59
William K <contact@williamk.ga>
2020-11-26 13:57:26 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,7 +3,7 @@ const { caseNumber } = require('../util/caseNumber.js');
 const { parseUser } = require('../util/parseUser.js');
 const settings = require('../settings.json');
 exports.run = async (client, message, args) => {
-  const userr = message.mentions.members.first() || message.guild.members.fetch(args[0]);
+  const userr = message.mentions.members.first() || message.guild.members.fetch(args[0]) || null;
   if (!userr) return message.reply('tag someone to kick next time before I kick you');
   parseUser(message, userr);
   if (userr.user.id == client.user.id) {
