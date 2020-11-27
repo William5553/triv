d1898e6f9618d47c3f24e4d0c3b8c7897ce0def1
William K <contact@williamk.ga>
2020-11-27 12:44:17 -0500

    Update parseUser.js

--- util/parseUser.js
+++ util/parseUser.js
@@ -2,6 +2,6 @@ exports.parseUser = (message, member) => {
   if (member.user.id === message.author.id)
     return message.reply("you can't do that to yourself, why did you try, you idiot");
   else if (member && member.roles && member.roles.highest.position >= message.member.roles.highest.position)
-      return message.channel.send('That member is higher or equal to you');
+    return message.channel.send('That member is higher or equal to you');
   return true;
 };
