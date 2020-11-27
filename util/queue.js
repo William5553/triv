0e19b840fff1ccf89b71230d2499c0d167283c57
William K <contact@williamk.ga>
2020-11-27 12:40:03 -0500

    Update queue.js

--- util/queue.js
+++ util/queue.js
@@ -4,7 +4,7 @@ module.exports = {
     const { channelID } = member.voice;
     const botChannel = member.guild.voice.channelID;
 
-    if (channelID == botChannel || member.id == settings.ownerid) {
+    if (channelID === botChannel || member.id === settings.ownerid) {
       return true;
     }
     return member.send('You need to join the voice channel first!').catch(member.client.logger.error);
