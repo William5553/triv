e147150eb1c3dc0fec4252913beed2e6381aa075
Glitch (triv) <none>
2020-11-30 15:30:49 +0000

    🙇💠 Updated with Glitch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (Number(process.version.slice(1).split(".")[0]) < 12) throw new Error("Node 12.0.0 or higher is required. Update Node on your system.");
+if (Number(process.version.slice(1).split('.')[0]) < 12) throw new Error('Node 12.0.0 or higher is required. Update Node on your system.');
 const { Client, Collection } = require('discord.js');
 const client = new Client({ disableMentions: 'everyone' });
 const fs = require('fs');
@@ -15,18 +15,18 @@ fs.readdir('./commands/', (err, files) => {
   if (err) client.logger.error(err);
   client.logger.log(`Loading a total of ${files.length} commands.`);
   files.forEach(file => {
-    if (!file.endsWith(".js")) return;
+    if (!file.endsWith('.js')) return;
     client.load(file);
   });
 });
 
-fs.readdir("./events/", (err, files) => {
+fs.readdir('./events/', (err, files) => {
   if (err) client.logger.error(err);
   client.logger.log(`Loading a total of ${files.length} events.`);
   files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    const eventName = file.split(".")[0];
-    client.logger.log(`Loading Event: ${eventName}`);
+    if (!file.endsWith('.js')) return;
+    const eventName = file.split('.')[0];
+    client.logger.log(`Loading Event: ${eventName}. 👌`);
     const event = require(`./events/${file}`);
     // Bind the client to any event, before the existing arguments
     // provided by the discord.js event. 
