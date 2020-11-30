c16e679478a43f1a8f25eed074524f28b412be97
William K <contact@williamk.ga>
2020-11-30 11:52:10 -0500

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -22,8 +22,8 @@ exports.run = async (client, message, args) => {
       .setColor(0x00ae86) // This sets the color of the embed, you can set this to anything if you look put a hex color picker, just make sure you put 0x infront of the hex
       .addField('Timezone', `UTC${location.timezone}`, true) // This is the first field, it shows the timezone, and the true means `inline`, you can read more about this on the official discord.js documentation
       .addField('Degree Type', location.degreetype, true) // This is the field that shows the degree type, and is inline
-      .addField('Temperature', `${current.temperature} Degrees`, true)
-      .addField('Feels Like', `${current.feelslike} Degrees`, true)
+      .addField('Temperature', `${current.temperature}° C`, true)
+      .addField('Feels Like', `${current.feelslike}° C`, true)
       .addField('Winds', current.winddisplay, true)
       .addField('Humidity', `${current.humidity}%`, true);
     message.channel.send({ embed });
