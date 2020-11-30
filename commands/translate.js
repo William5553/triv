3715011f8e9443464466271aa91daf675b3f536b
William K <contact@williamk.ga>
2020-11-30 13:06:54 -0500

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -156,11 +156,12 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['gt'],
-  permLevel: 0,
+  permLevel: 0
 };
 
 exports.help = {
   name: 'translate',
   description: 'Translates text using Google Translate',
   usage: 'translate [language from] [language to] [text]',
+  example: 'translate en french waffles'
 };
