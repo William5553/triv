d33978fbae2e15df8fcd0cc65b13cd5e5301b523
William K <contact@williamk.ga>
2020-11-30 13:07:12 -0500

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -163,5 +163,5 @@ exports.help = {
   name: 'translate',
   description: 'Translates text using Google Translate',
   usage: 'translate [language from] [language to] [text]',
-  example: 'translate en french waffles'
+  example: 'translate en french i like waffles'
 };
