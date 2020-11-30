4653c632db87dfca8a424f0069c03978ce7ed10b
William K <contact@williamk.ga>
2020-11-30 12:12:19 -0500

    Update functions.js

--- util/functions.js
+++ util/functions.js
@@ -117,7 +117,6 @@ module.exports = client => {
 
   // These 2 process methods will catch exceptions and give *more details* about the error and stack trace.
   process.on('uncaughtException', err => {
-    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
     client.logger.error(`Uncaught Exception: ${errorMsg}`);
   });
 
