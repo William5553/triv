f2343bd3bbe2c89fba1c76d1bc0fb1ecf93b1c36
Glitch (triv) <none>
2020-12-02 23:47:05 +0000

    aaaaaaaaaaaa

--- util/queue.js
+++ util/queue.js
@@ -1,10 +1,11 @@
 const settings = require('../settings.json');
 module.exports = {
   canModifyQueue(member) {
-    const { channelID } = member.voice;
-    const botChannel = member.guild.voice.channelID;
+    if (!member || !member.voice || !member.guild.voice) return member.client.logger.error('member.voice or member.guild.voice is not present');
+    const memChan = member.voice.channelID;
+    const botChan = member.guild.voice.channelID;
 
-    if (channelID === botChannel || member.id === settings.ownerid) {
+    if (memChan === botChan || member.id === settings.ownerid) {
       return true;
     }
     return member.send('You need to join the voice channel first!').catch(member.client.logger.error);
