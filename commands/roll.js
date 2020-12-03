c30c3e8343eec4c1f215dfffa635cf4a4cdd0baa
William K <contact@williamk.ga>
2020-12-03 14:08:02 -0500

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
   const sides = Number(args.slice(0).join(' ')) || 6;
-  var roll = Math.floor(Math.random() * sides) + 1;
+  const roll = Math.floor(Math.random() * sides) + 1;
   message.reply('You rolled ' + roll);
 };
 
@@ -8,11 +8,11 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: 0,
+  permLevel: 0
 };
 
 exports.help = {
   name: 'roll',
   description: 'Rolls a die',
-  usage: 'roll [dice sides]',
+  usage: 'roll [dice sides]'
 };
