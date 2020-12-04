797a8e89210e6ac003f74f035a239137f7206eeb
Glitch (triv) <none>
2020-12-04 07:09:42 +0000

    💺💝 Updated with Glitch

--- commands/madlibs.js
+++ commands/madlibs.js
@@ -1,40 +1,40 @@
 const libs = require('../assets/mad-libs.json');
 
 exports.run = async (client, msg) => {
-const current = client.games.get(msg.channel.id);
-		if (current) return msg.reply(`Please wait until the current game of \`${current.name}\` is finished.`);
-		client.games.set(msg.channel.id, { name: 'madlibs' });
-		try {
-			const lib = libs.random();
-			const choices = [];
-			for (const word of lib.needed) {
-				await msg.reply(`Give me a **${word}**.`);
-				const filter = res => {
-					if (res.author.id !== msg.author.id) return false;
-					if (!res.content || res.content.length > 12) {
-						msg.reply('only use a maximum of 12 characters per word.').catch(() => null);
-						return false;
-					}
-					return true;
-				};
-				const choice = await msg.channel.awaitMessages(filter, {
-					max: 1,
-					time: 120000
-				});
-				if (!choice.size) break;
-				choices.push(choice.first().content);
-			}
-			client.games.delete(msg.channel.id);
-			let finished = lib.text;
-			for (let i = 0; i < choices.length; i++) {
-				finished = finished.replaceAll(`{${i}}`, `**${choices[i]}**`);
-			}
-			return msg.say(finished);
-		} catch (err) {
-			client.games.delete(msg.channel.id);
-			throw err;
-		}
-	}
+  const current = client.games.get(msg.channel.id);
+  if (current) return msg.reply(`Please wait until the current game of \`${current.name}\` is finished.`);
+  client.games.set(msg.channel.id, { name: 'madlibs' });
+  try {
+    const lib = libs.random();
+    const choices = [];
+    for (const word of lib.needed) {
+      await msg.reply(`Give me a **${word}**.`);
+      const filter = res => {
+        if (res.author.id !== msg.author.id) return false;
+        if (!res.content || res.content.length > 12) {
+          msg.reply('only use a maximum of 12 characters per word.').catch(() => null);
+          return false;
+        }
+        return true;
+      };
+      const choice = await msg.channel.awaitMessages(filter, {
+        max: 1,
+        time: 120000
+      });
+      if (!choice.size) break;
+      choices.push(choice.first().content);
+    }
+    client.games.delete(msg.channel.id);
+    let finished = lib.text;
+    for (let i = 0; i < choices.length; i++) {
+      finished = finished.replaceAll(`{${i}}`, `**${choices[i]}**`);
+    }
+    return msg.channel.send(finished);
+  } catch (err) {
+    client.games.delete(msg.channel.id);
+    throw err;
+  }
+};
   
 exports.conf = {
   enabled: true,
