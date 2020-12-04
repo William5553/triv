68b5f1f7148e0d1be0efa3c67ab4e59c3d4b0cab
William K <contact@williamk.ga>
2020-12-04 11:20:12 -0500

    Update functions.js

--- util/functions.js
+++ util/functions.js
@@ -69,22 +69,22 @@ module.exports = client => {
       return false;
     }
   };
-  client.verify = async (channel, user, { time = 30000 } = {}) => {
-    const filter = res => {
-      const value = res.content.toLowerCase();
-      return (user ? res.author.id === user.id : true)
-				&& (yes.includes(value) || no.includes(value));
-    };
-    const verify = await channel.awaitMessages(filter, {
-      max: 1,
-      time
-    });
-    if (!verify.size) return 0;
-    const choice = verify.first().content.toLowerCase();
-    if (yes.includes(choice)) return true;
-    if (no.includes(choice)) return false;
-    return false;
-  };
+  client.verify = async (channel, user, { time = 30000, extraYes = [], extraNo = [] } = {}) => {
+		const filter = res => {
+			const value = res.content.toLowerCase();
+			return (user ? res.author.id === user.id : true)
+				&& (yes.includes(value) || no.includes(value) || extraYes.includes(value) || extraNo.includes(value));
+		};
+		const verify = await channel.awaitMessages(filter, {
+			max: 1,
+			time
+		});
+		if (!verify.size) return 0;
+		const choice = verify.first().content.toLowerCase();
+		if (yes.includes(choice) || extraYes.includes(choice)) return true;
+		if (no.includes(choice) || extraNo.includes(choice)) return false;
+		return false;
+	}
   /*
   MESSAGE CLEAN FUNCTION
   "Clean" removes @everyone pings, as well as tokens, and makes code blocks
