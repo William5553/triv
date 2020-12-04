d084fc472a14291a81791cdef24f60fec60ce417
Glitch (triv) <none>
2020-12-04 19:01:57 +0000

    🍳🎱 Updated with Glitch

--- commands/daysto.js
+++ commands/daysto.js
@@ -1,50 +1,50 @@
 const moment = require('moment');
 require('moment-duration-format');
-
+const {prefix} = require('../settings.json');
 const months = [
-	"january",
-	"february",
-	"march",
-	"april",
-	"may",
-	"june",
-	"july",
-	"august",
-	"september",
-	"october",
-	"november",
-	"december"
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december'
 ];
 
 exports.run = (client, message, args) => {
-if (args.length < 2) return message.reply(`${prefix}${exports.help.usage}`);
-const month = args[0];
-if (!validate(month)) return message.reply(`${month} is not a valid month`);
-else month = parse(month);
-const day = args[1];
-if (isNan(day) || day > 31 || day < 1) return message.reply(`${day} is not a valid day`);
-const now = new Date();
-		let year = now.getMonth() + 1 <= month ? now.getFullYear() : now.getFullYear() + 1;
-		if (month === now.getMonth() + 1 && now.getDate() >= day) ++year;
-		const future = new Date(year, month - 1, day);
-		const futureFormat = moment.utc(future).format('dddd, MMMM Do, YYYY');
-		const time = moment.duration(future - now);
-		const link = time.months() ? time.months() === 1 ? 'is' : 'are' : time.days() === 1 ? 'is' : 'are';
-		return message.channel.send(`There ${link} ${time.format('M [months and] d [days]')} until ${futureFormat}!`);
+  if (args.length < 2) return message.reply(`${prefix}${exports.help.usage}`);
+  let month = args[0];
+  if (!validate(month)) return message.reply(`${month} is not a valid month`);
+  else month = parse(month);
+  const day = args[1];
+  if (isNaN(day) || day > 31 || day < 1) return message.reply(`${day} is not a valid day`);
+  const now = new Date();
+  let year = now.getMonth() + 1 <= month ? now.getFullYear() : now.getFullYear() + 1;
+  if (month === now.getMonth() + 1 && now.getDate() >= day) ++year;
+  const future = new Date(year, month - 1, day);
+  const futureFormat = moment.utc(future).format('dddd, MMMM Do, YYYY');
+  const time = moment.duration(future - now);
+  const link = time.months() ? time.months() === 1 ? 'is' : 'are' : time.days() === 1 ? 'is' : 'are';
+  return message.channel.send(`There ${link} ${time.format('M [months and] d [days]')} until ${futureFormat}!`);
 };
 
 function validate(value) {
-		const num = Number.parseInt(value, 10);
-		if (num > 0 && num < 13) return true;
-		if (months.includes(value.toLowerCase())) return true;
-		return false;
-	}
+  const num = Number.parseInt(value, 10);
+  if (num > 0 && num < 13) return true;
+  if (months.includes(value.toLowerCase())) return true;
+  return false;
+}
 
-	function parse(value) {
-		const num = Number.parseInt(value, 10);
-		if (!Number.isNaN(num)) return num;
-		return months.indexOf(value.toLowerCase()) + 1;
-	}
+function parse(value) {
+  const num = Number.parseInt(value, 10);
+  if (!isNaN(num)) return num;
+  return months.indexOf(value.toLowerCase()) + 1;
+}
 
 exports.conf = {
   enabled: true,
@@ -55,6 +55,6 @@ exports.conf = {
 
 exports.help = {
   name: 'daysto',
-  description: "Responds with how many days there are until a certain date",
+  description: 'Responds with how many days there are until a certain date',
   usage: 'daysto [month] [day]'
 };
