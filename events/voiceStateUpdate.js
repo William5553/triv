75a10a7cfcef5c9da644c89bda1ed25214a88628
Glitch (triv) <none>
2020-12-04 03:16:11 +0000

    👯🌧 Updated with Glitch

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,13 +3,13 @@ module.exports = (client, oldState, newState) => {
   if (!queue) return;
 
   // if the bot has been kicked from the channel, destroy ytdl stream and remove the queue
-  if (newState.member.id === this.client.user.id && !newState.channelID) {
+  if (newState.member.id === client.user.id && !newState.channelID) {
     queue.stream.destroy();
     client.queue.delete(newState.guild.id);
   }
 
   // If the member leaves a voice channel
-  if (!oldState.channelID || newState.channelID) return; 
+  if (!oldState.channelID || !newState.channelID) return; 
         
   if (!queue.connection.channel.members.filter((member) => !member.user.bot).size === 0) return;
   if (!client.queue.has(queue.guildID)) return;
