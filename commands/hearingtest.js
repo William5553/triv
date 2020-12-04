a629fc84f27bae1600e3da48dcc88712e3f0b178
William K <contact@williamk.ga>
2020-12-03 23:18:05 -0500

    Create hearingtest.js

---
+++ commands/hearingtest.js
@@ -0,0 +1,36 @@
+const data = require('../assets/hearing-test');
+
+exports.run = async (client, msg) => {
+		try {
+			let age;
+			let range;
+			let previousAge = 'all';
+			let previousRange = 8;
+			for (const { age: dataAge, khz, file } of data) {
+				connection.play(path.join(__dirname, '..', 'assets', 'hearing-test', file));
+				await client.wait(3500);
+				const heard = await client.awaitReply(msg, 'Did you hear that sound? Reply with **[y]es** or **[n]o**.');
+				if (!heard || file === data[data.length - 1].file) {
+					age = previousAge;
+					range = previousRange;
+					break;
+				}
+				previousAge = dataAge;
+				previousRange = khz;
+			}
+			if (age === 'all') return msg.reply('Everyone should be able to hear that. You cannot hear.');
+			if (age === 'max') {
+				return msg.reply(stripIndents`
+					You can hear any frequency of which a human is capable.
+					The maximum frequency you were able to hear was **${range}000hz**.
+				`);
+			}
+			return msg.reply(stripIndents`
+				You have the hearing of someone **${Number.parseInt(age, 10) + 1} or older**.
+				The maximum frequency you were able to hear was **${range}000hz**.
+			`);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+  };
