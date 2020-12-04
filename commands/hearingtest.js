de5635e2669c046ac2a7b45471441d6bef642f77
William K <contact@williamk.ga>
2020-12-03 23:26:38 -0500

    Update hearingtest.js

--- commands/hearingtest.js
+++ commands/hearingtest.js
@@ -7,7 +7,7 @@ exports.run = async (client, msg) => {
 			let previousAge = 'all';
 			let previousRange = 8;
 			for (const { age: dataAge, khz, file } of data) {
-				connection.play(path.join(__dirname, '..', 'assets', 'hearing-test', file));
+				connection.play(path.join(__dirname, '..', 'assets', file));
 				await client.wait(3500);
 				const heard = await client.awaitReply(msg, 'Did you hear that sound? Reply with **[y]es** or **[n]o**.');
 				if (!heard || file === data[data.length - 1].file) {
@@ -20,12 +20,12 @@ exports.run = async (client, msg) => {
 			}
 			if (age === 'all') return msg.reply('Everyone should be able to hear that. You cannot hear.');
 			if (age === 'max') {
-				return msg.reply(stripIndents`
+				return msg.reply(`
 					You can hear any frequency of which a human is capable.
 					The maximum frequency you were able to hear was **${range}000hz**.
-				`);
+			`);
 			}
-			return msg.reply(stripIndents`
+			return msg.reply(`
 				You have the hearing of someone **${Number.parseInt(age, 10) + 1} or older**.
 				The maximum frequency you were able to hear was **${range}000hz**.
 			`);
