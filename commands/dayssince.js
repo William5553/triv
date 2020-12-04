9819337fd6a9ac164e9c1a5106447a87de008027
William K <contact@williamk.ga>
2020-12-04 13:44:31 -0500

    Create dayssince.js

---
+++ commands/dayssince.js
@@ -0,0 +1,61 @@
+const moment = require('moment');
+require('moment-duration-format');
+const { prefix } = require('../settings.json');
+
+const months = [
+	"january",
+	"february",
+	"march",
+	"april",
+	"may",
+	"june",
+	"july",
+	"august",
+	"september",
+	"october",
+	"november",
+	"december"
+];
+
+exports.run = (client, message, args) => {
+if (args.length < 3) return message.reply(`${prefix}${exports.help.usage}`);
+const month = args[0];
+if (!validate(month)) return message.reply(`${month} is not a valid month`);
+else month = parse(month);
+const day = args[1];
+if (isNan(day) || day > 31 || day < 1) return message.reply(`${day} is not a valid day`);
+const year = args[2];
+if (isNan(year)) return message.reply(`${year} is not a valid year`);
+const now = new Date();
+		const past = new Date(year, month - 1, day);
+		if (year < 100) past.setFullYear(year);
+		const pastFormat = moment.utc(past).format('dddd, MMMM Do, YYYY');
+		const time = moment.duration(now - past);
+		return message.channel.send(`There have been ${time.format('Y [years,] M [months and] d [days]')} since ${pastFormat}!`);
+    };
+    
+    function validate(value) {
+		const num = Number.parseInt(value, 10);
+		if (num > 0 && num < 13) return true;
+		if (months.includes(value.toLowerCase())) return true;
+		return false;
+	}
+
+	function parse(value) {
+		const num = Number.parseInt(value, 10);
+		if (!Number.isNaN(num)) return num;
+		return months.indexOf(value.toLowerCase()) + 1;
+	}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'dayssince',
+  description: "Responds with how many days there have been since a certain date",
+  usage: 'dayssince [month] [day] [year]'
+};
