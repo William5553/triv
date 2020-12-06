d9eca31b11f065d2068707d51c07ff1f27066364
Glitch (triv) <none>
2020-12-06 21:21:33 +0000

    uʍop ǝpısdn

---
+++ commands/upsidedown.js
@@ -0,0 +1,60 @@
+const esrever = require('esrever');
+const replaceD = {
+  y: 'ʎ',
+  w: 'ʍ',
+  v: 'ʌ',
+  u: 'n',
+  t: 'ʇ',
+  r: 'ɹ',
+  q: 'b',
+  p: 'd',
+  n: 'u',
+  m: 'ɯ',
+  k: 'ʞ',
+  j: 'ɾ',
+  i: 'ᴉ',
+  h: 'ɥ',
+  g: 'ƃ',
+  f: 'ɟ',
+  e: 'ǝ',
+  d: 'p',
+  c: 'ɔ',
+  b: 'q',
+  a: 'ɐ',
+  '!': '¡',
+  '?': '¿',
+  '&': '⅋',
+  ',': "'",
+  "'": ',',
+  '"': ',,',
+  '.': '˙',
+  '9': '6',
+  '7': 'ㄥ',
+  '6': '9',
+  '5': 'ϛ',
+  '4': 'ㄣ',
+  '3': 'Ɛ',
+  '2': 'ᄅ',
+  '1': 'Ɩ',
+  _: '‾'
+};
+exports.run = (client, message, args) => {
+  let frick = args.join(' ');
+  frick = frick.replace(/[a-z0-9&_.,!"?'/$]*/gi, match => {
+    return typeof replaceD[match] != 'undefined' ? replaceD[match] : match;
+  });
+  message.channel.send(esrever.reverse(frick));
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['fliptext', 'upsidedownify'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'upsidedown',
+  description: 'Flips text upside down',
+  usage: 'upsidedown [text]'
+};
