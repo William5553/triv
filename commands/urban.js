d4fa7d998db2d1ae21da6e16ec34bbabd6b693d0
Glitch (triv) <none>
2020-12-07 17:05:34 +0000

    urban

--- commands/urban.js
+++ commands/urban.js
@@ -4,20 +4,20 @@ exports.run = async (client, message, args) => {
   const word = args.join(' ');
   if (!word) return message.channel.send('Specify a word');
   const { body } = await request
-				.get('http://api.urbandictionary.com/v0/define')
-				.query({ term: word });
-			if (!body.list.length) return message.channel.send('Could not find any results');
-			const data = body.list[0];
-			const embed = new MessageEmbed()
-				.setColor(0x32A8F0)
-				.setAuthor('Urban Dictionary', 'https://i.imgur.com/Fo0nRTe.png', 'https://www.urbandictionary.com/')
-				.setURL(data.permalink)
-				.setTitle(data.word)
-				.setDescription(data.definition.replace(/\[|\]/g, '').substr(0,1200))
-				.setFooter(`👍 ${data.thumbs_up} 👎 ${data.thumbs_down}`)
-				.setTimestamp(new Date(data.written_on))
-				.addField('❯ Example', data.example ? data.example.replace(/\[|\]/g, '').substr(0,800) : 'None');
-			return message.channel.send(embed).catch(client.logger.error);
+    .get('http://api.urbandictionary.com/v0/define')
+    .query({ term: word });
+  if (!body.list.length) return message.channel.send('Could not find any results');
+  const data = body.list[0];
+  const embed = new MessageEmbed()
+    .setColor(0x32A8F0)
+    .setAuthor('Urban Dictionary', 'https://i.imgur.com/Fo0nRTe.png', 'https://www.urbandictionary.com/')
+    .setURL(data.permalink)
+    .setTitle(data.word)
+    .setDescription(data.definition.replace(/\[|\]/g, '').substr(0,1200))
+    .setFooter(`Author: ${data.author} | 👍 ${data.thumbs_up} 👎 ${data.thumbs_down}`)
+    .setTimestamp(new Date(data.written_on))
+    .addField('❯ Example', data.example ? data.example.replace(/\[|\]/g, '').substr(0,800) : 'None');
+  return message.channel.send(embed).catch(client.logger.error);
 };
 
 exports.conf = {
