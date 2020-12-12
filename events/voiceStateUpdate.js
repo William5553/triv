0bc2cc56e7633dbc3bb2fb070dba79978a664589
William K <contact@williamk.ga>
2020-12-12 18:07:11 -0500

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -9,12 +9,11 @@ module.exports = (client, oldState, newState) => {
   }
 
   // If the member leaves a voice channel
-  if (!oldState.channelID || !newState.channelID) return; 
+  if (!oldState.channelID || newState.channelID) return; 
         
   if (!(queue.connection.channel.members.filter((member) => !member.user.bot).size === 0)) return;
-  if (!client.queue.has(queue.guildID)) return;
   // Disconnect from the voice channel
-  client.queue.connection.channel.leave();
+  queue.connection.channel.leave();
   // Delete the queue
   client.queue.delete(oldState.guild.id);          
 };
