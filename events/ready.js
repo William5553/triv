5c607a3f22d0ca6ba6cf9f8e3ed8740b85fddf54
Glitch (triv) <none>
2020-12-12 21:26:24 +0000

    🏣👩‍👩‍👦 Updated with Glitch

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
 const settings = require('../settings.json');
 module.exports = client => {
   client.logger.log(client.user.tag, 'ready');
-  if (client.guilds.cache.size >= 2) {
-    client.user.setActivity(`${settings.prefix}help | ${client.guilds.cache.size} guilds`, { type: 'LISTENING' });
-  } else {
+  if (client.guilds.cache.size === 1) {
     client.user.setActivity(`${settings.prefix}help | ${client.guilds.cache.size} guild`, { type: 'LISTENING' });
+  } else {
+    client.user.setActivity(`${settings.prefix}help | ${client.guilds.cache.size} guilds`, { type: 'LISTENING' });
   }
 };
