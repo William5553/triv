85f2e5c005d8da523032b96eb2a790f700fb104a
Glitch (triv) <none>
2020-12-13 04:09:14 +0000

    💖👞 Updated with Glitch

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,33 +1,11 @@
 const path = require('path');
 
 exports.run = (client, message, args) => {
-  const question = args.slice(0).join(' ');
-  const responses = [
-    'Explode',
-    'It is certain',
-    'It is decidedly so',
-    'Without a doubt',
-    'Yes definitely',
-    'You may rely on it',
-    'As I see it, yes',
-    'Most likely',
-    'Outlook good',
-    'Yes',
-    'Signs point to yes',
-    'Reply hazy try again',
-    'Ask again later',
-    'Better not tell you now',
-    'Cannot predict now',
-    'Concentrate and ask again',
-    "Don't count on it",
-    'My reply is no',
-    'My sources say no',
-    'Outlook not so good',
-    'Very doubtful'
-  ];
+  const question = args.join(' '),
+    responses = require('../assets/8ball.json');
 
   if (question.length < 1) return message.reply('ask me a question moron');
-  const response = `${responses[Math.floor(Math.random() * responses.length)]}`;
+  const response = responses.random();
   if (response === 'Explode') {
     message.channel.send({ files: [path.join(process.cwd(), 'assets', 'explode8ball.png')] }).catch(client.logger.error);
   } else {
@@ -39,11 +17,11 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: 0,
+  permLevel: 0
 };
 
 exports.help = {
   name: '8ball',
   description: 'Asks the magic 8 ball a question',
-  usage: '8ball [question]',
+  usage: '8ball [question]'
 };
