c0188f8b774d54d862cf8219c3bc818d4f5dad95
William K <contact@williamk.ga>
2020-12-13 17:10:09 -0500

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,10 +2,9 @@ const { MessageEmbed } = require('discord.js');
 module.exports = (client, oldMessage, newMessage) => {
   if (!newMessage.content || !oldMessage.content || !newMessage.guild) return;
   const logs = newMessage.guild.channels.cache.find(channel => channel.name === 'bot-logs');
-  if (newMessage.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
+  if (newMessage.guild.me.hasPermission('MANAGE_CHANNELS') && !logs)
     newMessage.guild.channels.create('bot-logs', { type: 'text' });
-  }
-
+  
   if (logs) logs.send(new MessageEmbed()
     .setTitle('**Message Edited**')
     .setAuthor(`@${newMessage.author.tag} - #${newMessage.channel.name}`, newMessage.author.avatarURL())
