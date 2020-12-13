60223d44f5736d0c1694f3898816e4c715d8c28b
William K <contact@williamk.ga>
2020-12-12 21:59:04 -0500

    Update guilds.js

--- commands/guilds.js
+++ commands/guilds.js
@@ -1,12 +1,12 @@
 const { MessageEmbed } = require('discord.js');
 exports.run = (client, message) => {
-  const embed = new MessageEmbed()
+  message.channel.send(new MessageEmbed()
     .setColor(0x00ff5c)
     .setAuthor(client.user.username, client.user.avatarURL())
-    .setDescription(`**${client.guilds.cache.size} guild(s):**\n\n*${client.guilds.cache.map(g => g.name).join('\n')}*`)
+    .setDescription(`**${client.guilds.cache.size} guild(s):**\n\n*${client.guilds.cache.map(g => `${g.name} - ${g.id}`).join('\n')}*`)
     .setURL('https://discordapp.com/oauth2/authorize?client_id=340942145051426828&scope=bot&permissions=536308991')
     .setTimestamp();
-  message.channel.send(embed);
+  );
 };
 
 exports.conf = {
