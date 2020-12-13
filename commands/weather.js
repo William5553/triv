443ec16dec76d99559d769423246255800f1d3dd
Glitch (triv) <none>
2020-12-13 05:55:47 +0000

    🏆🐽 Updated with Glitch

--- commands/weather.js
+++ commands/weather.js
@@ -1,13 +1,13 @@
 exports.run = async (client, message, args) => {
-  const weather = require('weather-js'),
+  const { find } = require('weather-js'),
     { MessageEmbed } = require('discord.js');
 
-  weather.find({ search: args.join(' '), degreeType: 'C' }, (err, result) => {
+  find({ search: args.join(' '), degreeType: 'C' }, (err, result) => {
     if (err) message.channel.send(err);
 
     if (result.length === 0) return message.channel.send('**Please enter a valid location.**');
 
-    var current = result[0].current,
+    const current = result[0].current,
       location = result[0].location;
 
     const embed = new MessageEmbed()
@@ -15,6 +15,7 @@ exports.run = async (client, message, args) => {
       .setAuthor(`Weather for ${current.observationpoint}`) // This shows the current location of the weather.
       .setThumbnail(current.imageUrl) // This sets the thumbnail of the embed
       .setColor(0x00ae86) // This sets the color of the embed, you can set this to anything if you look put a hex color picker, just make sure you put 0x infront of the hex
+      .setFooter(`Observed at ${current.observationtime}`)
       .addField('Timezone', `UTC${location.timezone}`, true) // the true means `inline`
       .addField('Temperature', `${current.temperature}° C`, true)
       .addField('Feels Like', `${current.feelslike}° C`, true)
