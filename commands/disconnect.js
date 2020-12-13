05d62bb3d7e9c2098dd6ba079264b1529213c692
Glitch (triv) <none>
2020-12-13 21:47:25 +0000

    🚴📈 Updated with Glitch

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -1,17 +1,18 @@
 const texts = require('../assets/shutdown.json');
 
-exports.run = async (client, msg) => {
+exports.run = async (client, message) => {
   const games = client.games.size;
   let areIs = 'are';
   if (games > 0) {
     const currentString = `${games} game${games > 1 ? 's' : ''}`;
     if (games === 1) areIs = 'is';
 			
-    await msg.reply(`there ${areIs} currently **${currentString}**. Are you sure?`);
-    const verification = await client.verify(msg.channel, msg.author);
-    if (!verification) return msg.channel.send('Aborted restart.');
+    await message.reply(`there ${areIs} currently **${currentString}**. Are you sure?`);
+    const verification = await client.verify(message.channel, message.author);
+    if (!verification) return message.channel.send('Aborted restart.');
   }
   await msg.channel.send(texts.random());
+  client.logger.log(`${msg.author.tag} is restarting the bot`);
   client.destroy();
   process.exit(0);
 };
