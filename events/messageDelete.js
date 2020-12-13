d8342f7fff4f905a773b1f9fa0ed6e269d0cf995
William K <contact@williamk.ga>
2020-12-13 17:11:27 -0500

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -5,17 +5,17 @@ module.exports = (client, message) => {
   if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs)
     message.guild.channels.create('bot-logs', { type: 'text' });
 
-  const embed = new MessageEmbed()
-    .setTitle('**Message Deleted**')
-    .setAuthor(`@${message.author.tag} - #${message.channel.name}`, message.author.avatarURL())
-    .setFooter(`User ID: ${message.author.id}`)
-    .setTimestamp()
-    .setDescription(message.content)
-    .setColor('0xEB5234');
   if (logs) {
-    logs.send(embed);
+    logs.send(new MessageEmbed()
+      .setTitle('**Message Deleted**')
+      .setAuthor(`@${message.author.tag} - #${message.channel.name}`, message.author.avatarURL())
+      .setFooter(`User ID: ${message.author.id}`)
+      .setTimestamp()
+      .setDescription(message.content)
+      .setColor('0xEB5234')
+    );
     logs.updateOverwrite(message.channel.guild.roles.everyone, {
-      SEND_MESSAGES: false,
+      SEND_MESSAGES: false
     });
   }
 };
