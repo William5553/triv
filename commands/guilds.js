43ec6deb5993a6946e1d6398433232a72c346b46
Glitch (triv) <none>
2020-12-13 03:00:39 +0000

    🌓📮 Updated with Glitch

--- commands/guilds.js
+++ commands/guilds.js
@@ -5,7 +5,7 @@ exports.run = (client, message) => {
     .setAuthor(client.user.username, client.user.avatarURL())
     .setDescription(`**${client.guilds.cache.size} guild(s):**\n\n*${client.guilds.cache.map(g => `${g.name} - ${g.id}`).join('\n')}*`)
     .setURL('https://discordapp.com/oauth2/authorize?client_id=340942145051426828&scope=bot&permissions=536308991')
-    .setTimestamp();
+    .setTimestamp()
   );
 };
 
