e78ff6e5a2b156f18f7722b836baf4486947d751
Glitch (triv) <none>
2020-12-13 05:48:00 +0000

    💘🗽 Updated with Glitch

--- commands/weather.js
+++ commands/weather.js
@@ -15,8 +15,7 @@ exports.run = async (client, message, args) => {
       .setAuthor(`Weather for ${current.observationpoint}`) // This shows the current location of the weather.
       .setThumbnail(current.imageUrl) // This sets the thumbnail of the embed
       .setColor(0x00ae86) // This sets the color of the embed, you can set this to anything if you look put a hex color picker, just make sure you put 0x infront of the hex
-      .addField('Timezone', `UTC${location.timezone}`, true) // This is the first field, it shows the timezone, and the true means `inline`, you can read more about this on the official discord.js documentation
-      .addField('Degree Type', location.degreetype, true) // This is the field that shows the degree type, and is inline
+      .addField('Timezone', `UTC${location.timezone}`, true) // the true means `inline`
       .addField('Temperature', `${current.temperature}° C`, true)
       .addField('Feels Like', `${current.feelslike}° C`, true)
       .addField('Winds', current.winddisplay, true)
