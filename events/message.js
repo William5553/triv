93bd27780fafba18b837f95b366d607c17fd51fd
William Kang <contact@williamk.ga>
2020-12-14 13:56:54 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,7 +3,7 @@ module.exports = (client, message) => {
   const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
   if (message.content.match(prefixMention))
     return message.reply(`my prefix on this guild is \`${client.settings.prefix}\``);
-  if (!message.content.startsWith(settings.prefix)) return;
+  if (!message.content.startsWith(client.settings.prefix)) return;
   const command = message.content.split(' ')[0].slice(client.settings.prefix.length).toLowerCase();
   const args = message.content.split(' ').slice(1);
   let cmd;
@@ -14,13 +14,15 @@ module.exports = (client, message) => {
   if (cmd) {
     if (!message.guild) {
       if (!cmd.conf.guildOnly) {
-        if (cmd.conf.permLevel >= 10 && message.author.id !== client.settings.ownerid) return message.reply("you don't have the perms for that");
+        if (cmd.conf.permLevel >= 10 && message.author.id !== client.settings.ownerid)
+          return message.reply("you don't have the perms for that");
         return cmd.run(client, message, args, 4);
       } else if (cmd.conf.guildOnly)
         return message.reply('that command can only be used in a guild, get some friends.');
     }
     const perms = client.elevation(message);
-    if (perms < cmd.conf.permLevel) return message.reply("you don't have the perms for that");
+    if (perms < cmd.conf.permLevel)
+      return message.reply("you don't have the perms for that");
     cmd.run(client, message, args, perms);
   }
 };
