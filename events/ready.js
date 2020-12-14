4ff8e04d92e1b98963b5190d88fc45d6078f904b
William Kang <contact@williamk.ga>
2020-12-14 13:31:17 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,7 @@
-const settings = require('../settings.json');
 module.exports = client => {
   client.logger.log(client.user.tag, 'ready');
-  if (client.guilds.cache.size === 1) {
-    client.user.setActivity(`${settings.prefix}help | ${client.guilds.cache.size} guild`, { type: 'LISTENING' });
-  } else {
-    client.user.setActivity(`${settings.prefix}help | ${client.guilds.cache.size} guilds`, { type: 'LISTENING' });
-  }
+  if (client.guilds.cache.size === 1)
+    client.user.setActivity(`${client.settings.prefix}help | ${client.guilds.cache.size} guild`, { type: 'LISTENING' });
+  else
+    client.user.setActivity(`${client.settings.prefix}help | ${client.guilds.cache.size} guilds`, { type: 'LISTENING' });
 };
