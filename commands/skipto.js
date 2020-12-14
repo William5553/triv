d30faef9d241b8f084863cb79688af3e53ab8f2d
William Kang <contact@williamk.ga>
2020-12-14 12:49:41 -0500

    Update skipto.js

--- commands/skipto.js
+++ commands/skipto.js
@@ -1,17 +1,16 @@
-const { canModifyQueue } = require('../util/queue'),
-  settings = require('../settings.json');
+const { canModifyQueue } = require('../util/queue');
 
 exports.run = (client, message, args) => {
-  if (!args.length) return message.reply(`${settings.prefix}${exports.help.usage}`).catch(client.logger.error);
+  if (!args.length) return message.reply(`${client.settings.prefix}${exports.help.usage}`).catch(client.logger.error);
 
-  if (isNaN(args[0])) return message.reply(`${settings.prefix}${exports.help.usage}`).catch(client.logger.error);
+  if (isNaN(args[0])) return message.reply(`${client.settings.prefix}${exports.help.usage}`).catch(client.logger.error);
 
   const queue = client.queue.get(message.guild.id);
-  if (!queue) return message.channel.send('There is no queue.').catch(client.logger.error);
+  if (!queue) return message.reply('there is nothing playing').catch(client.logger.error);
   if (!canModifyQueue(message.member)) return;
 
   if (args[0] > queue.songs.length)
-    return message.reply(`The queue is only ${queue.songs.length} songs long!`).catch(client.logger.error);
+    return message.reply(`the queue is only ${queue.songs.length} songs long`).catch(client.logger.error);
 
   queue.playing = true;
   if (queue.loop) {
@@ -22,7 +21,7 @@ exports.run = (client, message, args) => {
     queue.songs = queue.songs.slice(args[0] - 2);
   }
   queue.connection.dispatcher.end();
-  queue.textChannel.send(`${message.author} ⏭ skipped ${args[0] - 1} songs`).catch(client.logger.error);
+  queue.textChannel.send(`${message.author} ⏭ skipped ${Number(args[0]) - 1} songs`).catch(client.logger.error);
 };
 
 exports.conf = {
