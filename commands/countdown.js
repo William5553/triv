8905dfc9541894585c8f425857758b9fb5c54c0d
William Kang <contact@williamk.ga>
2020-12-15 23:33:20 -0500

    Update countdown.js

--- commands/countdown.js
+++ commands/countdown.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-  if (!args || isNaN(args[0]) return message.reply('tell me a number next time, moron');
+  if (!args || isNaN(args[0])) return message.reply('tell me a number next time, moron');
   if (Number(args[0]) > 3600) return message.reply('the countdown cannot be for more than an hour');
   message.channel.send('Countdown started');
   await client.wait(Number(args[0]) * 1000);
