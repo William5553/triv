d74ed776cbd89b616900c591b6ecdbf76ec821e7
William Kang <contact@williamk.ga>
2020-12-17 17:51:18 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args) => {
   const user = message.mentions.users.first();
   await message.delete(); // delete the command message, so it doesn't interfere with the messages we are going to delete.
   let mgct = Number(args[0]);
-  if (!mgct || isNaN(mgct) || mgct < 1) return message.channel.send(`Usage: ${client.settings.prefix}${exports.help.usage}`);
+  if (!mgct || isNaN(mgct) || mgct < 1) return message.channel.send(`Usage: ${client.settings.prefix}${exports.help.usage}`).then(m => m.delete({ timeout: 4500 }));
   if (mgct > 100) mgct = 100;
   message.channel.messages
     .fetch({ limit: 100 })
@@ -13,7 +13,7 @@ exports.run = async (client, message, args) => {
         messages = messages.array().slice(0, mgct);
       message.channel
         .bulkDelete(messages, true)
-        .catch(e => client.logger.log(e.stack ? e.stack : e));
+        .catch(e => message.channel.send(e.message ? e.message : e));
     });
 };
 
