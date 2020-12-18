f072ad0393d92d209a4d067b775232dfb181aeff
William Kang <contact@williamk.ga>
2020-12-18 00:06:35 -0500

    Update cloc.js

--- commands/cloc.js
+++ commands/cloc.js
@@ -18,11 +18,14 @@ exports.run = async (client, message) => {
   const cloc = await clc();
   return message.channel.send(new MessageEmbed()
     .setColor(0x00AE86)
+    .setTitle(client.user.username)
+    .setDescription('[View source code](https://github.com/William5553/triv)')
     .addField(`❯ JavaScript: ${cloc.JavaScript.nFiles} files`, `${cloc.JavaScript.code} lines`, true)
     .addField(`❯ JSON: ${cloc.JSON.nFiles} files`, `${cloc.JSON.code} lines`, true)
     .addField('\u200B', '\u200B', true)
     .addField(`❯ Total: ${cloc.SUM.nFiles} files`, `${cloc.SUM.code} lines`, true)
     .addField('\u200B', '\u200B', true)
+    
   );
 };
   
