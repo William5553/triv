198b045ed9c224358366918b573e85b935cda08c
William Kang <contact@williamk.ga>
2020-12-17 22:39:27 -0500

    Update quizduel.js

--- commands/quizduel.js
+++ commands/quizduel.js
@@ -5,16 +5,12 @@ const request = require('node-superfetch'),
 exports.run = async (client, msg, args) => {
   const current = client.games.get(msg.channel.id);
   if (current)
-    return msg.reply(
-      `Please wait until the current game of \`${current.name}\` is finished.`
-    );
+    return msg.reply(`Please wait until the current game of \`${current.name}\` is finished.`);
   client.games.set(msg.channel.id, { name: 'quizduel' });
   try {
     const players = Number(args[0]);
     if (!players || isNaN(players) || players > 100 || players < 1)
-      return msg.channel.send(
-        `Usage: ${client.settings.prefix}${exports.help.usage}`
-      );
+      return msg.channel.send(`Usage: ${client.settings.prefix}${exports.help.usage}`);
     const awaitedPlayers = await awaitPlayers(msg, players);
     let turn = 0;
     const pts = new Collection();
@@ -31,22 +27,12 @@ exports.run = async (client, msg, args) => {
       ++turn;
       const question = questions[0];
       questions.shift();
-      await msg.channel.send(`
-					**${turn}. ${question.category}**
-					${question.question}
-					${question.answers
-    .map((answer, i) => `**${choices[i]}.** ${answer}`)
-    .join('\n')}
-				`);
+      await msg.channel.send(`**${turn}. ${question.category}**\n${question.question}\n${question.answers.map((answer, i) => `**${choices[i]}.** ${answer}`).join('\n')}`);
       const filter = res => {
         if (!awaitedPlayers.includes(res.author.id)) return false;
         const answer = res.content.toUpperCase();
         if (choices.includes(answer)) {
-          if (
-            msg.channel
-              .permissionsFor(res.author)
-              .has(['ADD_REACTIONS', 'READ_MESSAGE_HISTORY'])
-          )
+          if (msg.channel.permissionsFor(res.author).has(['ADD_REACTIONS', 'READ_MESSAGE_HISTORY']))
             msg.react('✅');
           return true;
         }
@@ -57,9 +43,7 @@ exports.run = async (client, msg, args) => {
         time: 30000
       });
       if (!msgs.size) {
-        await msg.channel.send(
-          `No answers? Well, it was **${question.correct}**.`
-        );
+        await msg.channel.send(`No answers? Well, it was **${question.correct}**.`);
         if (lastTurnTimeout) {
           break;
         } else {
@@ -74,35 +58,21 @@ exports.run = async (client, msg, args) => {
           id: res.author.id
         };
       });
-      const correct = answers.filter(
-        answer => answer.answer === question.correct
-      );
+      const correct = answers.filter(answer => answer.answer === question.correct);
       for (const answer of correct) {
         const player = pts.get(answer.id);
-        if (correct[0].id === answer.id) player.points += 75;
-        else player.points += 50;
+        if (correct[0].id === answer.id)
+	  player.points += 75;
+        else
+	 player.points += 50;
       }
-      await msg.channel.send(`
-      It was... **${question.correct}**!
-      _Fastest Guess: ${
-  correct.length
-    ? `${pts.get(correct[0].id).user.tag} (+75 pts)`
-    : 'No One...'
-}_
-					${questions.length ? '_Next round starting in 5 seconds..._' : ''}
-				`);
+      await msg.channel.send(`It was... **${question.correct}**!\n_Fastest Guess: ${correct.length ? `${pts.get(correct[0].id).user.tag} (+75 pts)` : 'no one'}_\n${questions.length ? '_Next round starting in 5 seconds..._' : ''}`);
       if (lastTurnTimeout) lastTurnTimeout = false;
       if (questions.length) await client.wait(5000);
     }
     client.games.delete(msg.channel.id);
     const winner = pts.sort((a, b) => b.points - a.points).first().user;
-    return msg.channel.send(`
-				Congrats, ${winner}!
-				__**Top 10:**__
-				${makeLeaderboard(pts)
-    .slice(0, 10)
-    .join('\n')}
-			`);
+    return msg.channel.send(`Congrats, ${winner}!\n__**Top 10:**__\n${makeLeaderboard(pts).slice(0, 10).join('\n')}`);
   } catch (err) {
     client.games.delete(msg.channel.id);
     throw err;
@@ -115,7 +85,7 @@ async function fetchQuestions() {
     type: 'multiple',
     encode: 'url3986'
   });
-  if (!body.results) return this.fetchQuestions();
+  if (!body.results) return fetchQuestions();
   const questions = body.results;
   return questions.map(question => {
     const answers = question.incorrect_answers.map(answer =>
@@ -155,10 +125,7 @@ function makeLeaderboard(pts) {
 async function awaitPlayers(msg, max, min = 1) {
   if (max === 1) return [msg.author.id];
   const addS = min - 1 === 1 ? '' : 's';
-  await msg.channel.send(
-    `You will need at least ${min - 1} more player${addS} (at max ${max -
-      1}). To join, type \`join game\`.`
-  );
+  await msg.channel.send(`You will need at least ${min - 1} more player${addS} (at max ${max - 1}). To join, type \`join game\`.`);
   const joined = [];
   joined.push(msg.author.id);
   const filter = res => {
@@ -166,14 +133,9 @@ async function awaitPlayers(msg, max, min = 1) {
     if (joined.includes(res.author.id)) return false;
     if (res.content.toLowerCase() !== 'join game') return false;
     joined.push(res.author.id);
-    if (
-      msg.channel
-        .permissionsFor(res.author)
-        .has(['ADD_REACTIONS', 'READ_MESSAGE_HISTORY'])
-    )
+    if (msg.channel.permissionsFor(res.author).has(['ADD_REACTIONS', 'READ_MESSAGE_HISTORY']))
       msg.react('✅');
   };
-
   const verify = await msg.channel.awaitMessages(filter, {
     max: max - 1,
     time: 60000
