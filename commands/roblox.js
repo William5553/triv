88e7efab9f87c0130ae2cabf7563e5314e9beb2a
William Kang <contact@williamk.ga>
2020-12-17 19:40:41 -0500

    Update roblox.js

--- commands/roblox.js
+++ commands/roblox.js
@@ -11,7 +11,7 @@ exports.run = async (client, message) => {
   } catch (e) {
     m.edit('I was unable to find a Roblox account linked with that user');
   }
-  m.edit(new MessageEmbed()
+  m.edit('', new MessageEmbed()
     .setTitle(data.body.robloxUsername)
     .setDescription(`https://roblox.com/users/${data.body.robloxId}/profile`)
     .setColor(0x00ae86)
