c3ba1d6a7ab8c4c1adfd1b1b152145422eced9ea
William Kang <contact@williamk.ga>
2020-12-17 19:36:02 -0500

    Update roblox.js

--- commands/roblox.js
+++ commands/roblox.js
@@ -4,13 +4,14 @@ const fetch = require('node-superfetch'),
 exports.run = async (client, message) => {
   const user = message.mentions.users.first();
   if (!user) return message.channel.send(`Usage: ${client.settings.prefix}${exports.help.usage}`);
+  const m = await message.channel.send('Getting...');
   let data;
   try {
     data = await fetch.get(`https://verify.eryn.io/api/user/${user.id}`).body;
   } catch (e) {
-    message.channel.send('I was unable to find a Roblox account linked with that user');
+    m.edit('I was unable to find a Roblox account linked with that user');
   }
-  message.channel.send(new MessageEmbed()
+  m.edit(new MessageEmbed()
     .setTitle(data.body.robloxUsername)
     .setDescription(`https://roblox.com/users/${data.body.robloxId}/profile`)
     .setColor(0x00ae86)
