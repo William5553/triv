05bdaad17638638574db832d4b59524cfb33aa35
William Kang <contact@williamk.ga>
2020-12-22 00:23:14 -0500

    Update gender.js

--- commands/gender.js
+++ commands/gender.js
@@ -1,4 +1,5 @@
-const request = require('node-superfetch');
+const request = require('node-superfetch'),
+  { MessageEmbed } = require('discord.js');
 
 exports.run = async (client, msg, args) => {
   try {
@@ -10,7 +11,13 @@ exports.run = async (client, msg, args) => {
     if (!body.gender) return msg.channel.send(`I have no idea what gender ${body.name} is.`);
     return msg.channel.send(`I'm ${Math.round(body.probability * 100)}% sure ${body.name} is a ${body.gender} name.`);
   } catch (err) {
-    return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+    return msg.channel.send(new MessageEmbed()
+      .setColor('RED')
+      .setTimestamp()
+      .setTitle('Please report this on GitHub')
+      .setURL('https://github.com/william5553/triv/issues')
+      .setDescription(`StackTrace: \n\`\`\`${err.stack}\`\`\``)
+      .addField('Command:', `${msg.content}`));
   }
 };
     
