f4b796d27af510bc2e0527a5dfece4688edd8548
William Kang <random@williamk.ga>
2021-01-06 21:27:51 -0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -48,7 +48,7 @@ exports.run = async (client, message, args) => {
           .setColor(0x00ae86)
           .setTimestamp()
           .setDescription(
-            `**Action:** Unmute\n**Target:** ${userr.user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}\n**User ID:** ${userr.user.tag}`
+            `**Action:** Unmute\n**Target:** ${userr.user.tag}\n**Moderator:** ${message.author.tag}\n**User ID:** ${userr.user.tag}`
           )
           .setFooter(`ID ${caseNum}`)).catch(client.logger.error);
       })
@@ -79,5 +79,5 @@ exports.conf = {
 exports.help = {
   name: 'mute',
   description: 'Toggles the mute of a member',
-  usage: 'mute [user] [reason] OR unmute [user] [reason]'
+  usage: 'mute [user] [reason] OR unmute [user]'
 };
