6df7e222a4ba63edb533a562c2068a49fb883e25
Glitch (triv) <none>
2021-01-08 16:34:25 +0000

    🚶👀 Updated with Glitch

---
+++ commands/hastebin.js
@@ -0,0 +1,30 @@
+const fetch = require('node-superfetch'),
+  { MessageEmbed } = require('discord.js'),
+  
+  EscapeMarkdown = (text) => text.replace(/(\*|~+|`)/g, ''),
+  baseURL = 'https://hastebin.com';
+
+exports.run = async (client, message, args) => {
+  const { body } = await fetch
+    .post(`${baseURL}/documents`)
+    .send(EscapeMarkdown(args.join(' ')));
+
+    
+  message.channel.send(new MessageEmbed()
+    .setTitle('Hastebin')
+    .setColor('GREEN')
+    .setDescription(`${baseURL}/${body.key}`));
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['hb'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'hastebin',
+  description: 'Upload text to hastebin',
+  usage: 'hastebin [text]'
+};
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = client => {
-  client.logger.log(client.user.tag, 'ready');
+  client.logger.log(`User: ${client.user.tag} Prefix: ${client.settings.prefix}`, 'ready');
   if (client.guilds.cache.size === 1)
     client.user.setActivity(`${client.settings.prefix}help | ${client.guilds.cache.size} guild`, { type: 'LISTENING' });
   else
