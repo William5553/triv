9606840234350b5696acc73759cf4b7092ff1d85
William Kang <random@williamk.ga>
2021-01-18 13:09:14 -0500

    Update functions.js

--- util/functions.js
+++ util/functions.js
@@ -1,4 +1,4 @@
-const yes = ['true', 'yes', 'y', 'ye', 'yeah', 'yup', 'yea', 'ya', 'yas', 'yuh', 'yis', 'hai', 'da', 'si', 'sí', 'oui', 'はい', 'correct', 'perhaps', 'absolutely', 'sure'];
+const yes = ['true', 'yes', 'y', 'ye', 'yeah', 'yup', 'yea', 'ya', 'yas', 'yuh', 'fosho', 'yis', 'hai', 'da', 'si', 'sí', 'oui', 'はい', 'correct', 'perhaps', 'absolutely', 'sure'];
 const no = ['false', 'no', 'n', 'nah', 'eat shit', 'nah foo', 'nope', 'nop', 'iie', 'いいえ', 'non', 'fuck off', 'absolutely not'];
 module.exports = client => {
   client.load = async command => {
