d817f8896d0c4e4e3166762357c938fe6483fcd3
William Kang <random@williamk.ga>
2021-01-18 20:49:07 -0500

    Update queue.js

--- util/queue.js
+++ util/queue.js
@@ -1,13 +1,14 @@
 module.exports = {
   canModifyQueue(member) {
     if (!member || !member.voice || !member.guild.voice) return member.client.logger.error('member.voice or member.guild.voice is not present');
+    const client = member.client;
     const memChan = member.voice.channelID;
     const botChan = member.guild.voice.channelID;
     
-    if (member.client.blacklist.includes(member.user.id))
-      return member.send('You are temporarily blacklisted').catch(member.client.logger.error);
-    if (memChan === botChan || member.id === member.client.settings.owner_id)
+    if (client.blacklist.includes(member.user.id))
+      return member.send('You are temporarily blacklisted').catch(client.logger.error);
+    if (memChan === botChan || member.id === client.settings.owner_id)
       return true;
-    return member.send('You need to join the voice channel first!').catch(member.client.logger.error);
+    return member.send('You need to join the voice channel first!').catch(client.logger.error);
   }
 };
