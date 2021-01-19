d7ed803c79691687995408a344e27466c40783e4
William Kang <random@williamk.ga>
2021-01-18 20:54:52 -0500

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -3,7 +3,7 @@ const { canModifyQueue } = require('../util/queue');
 exports.run = (client, message) => {
   const queue = client.queue.get(message.guild.id);
   if (!queue) return message.reply('there is nothing playing.').catch(client.logger.error);
-  if (!canModifyQueue(message.member)) return;
+  if (canModifyQueue(message.member) != true) return;
   queue.loop = !queue.loop;
   return queue.textChannel.send(`Loop is now ${queue.loop ? '**on**' : '**off**'}`).catch(client.logger.error);
 };
