643de0a40ee6607cbdee64efea3c894e79cd7ab9
William Kang <random@williamk.ga>
2021-02-08 09:28:06 -0500

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,11 +1,13 @@
 const fs = require('fs'),
   path = require('path');
 module.exports = guild => {
-  const warnings = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'warnings.json'), 'utf-8'));
-
-  if (warnings[guild.id]) {
-    delete warnings[guild.id];
+  let warnings;
+  try {
+    warnings = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'warnings.json'), 'utf-8'));
+  } catch {
+    return;
   }
+  if (warnings[guild.id]) delete warnings[guild.id];
   fs.writeFile(path.resolve(process.cwd(), 'warnings.json'), JSON.stringify(warnings), err => {
     if (err) guild.client.logger.error(err);
   });
