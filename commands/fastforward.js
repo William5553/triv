e0d2fc830ccd3d0cf1c20b2041555245d54a9834
William Kang <random@williamk.ga>
2021-02-11 02:25:39 -0500

    Update fastforward.js

--- commands/fastforward.js
+++ commands/fastforward.js
@@ -5,7 +5,9 @@ exports.run = async (client, message, args) => {
   if (!queue || !queue.connection) return message.reply('nothing is playing');
   if (isNaN(args[0])) return message.reply(`Usage: ${client.settings.prefix}${exports.help.usage}`);
   if (canModifyQueue(message.member) != true) return;
+  if (Number(args[0]) > 1000) return message.reply('you can only fast forward up to 1000 seconds.');
   queue.additionalStreamTime += Number(args[0])*1000;
+  if (queue.connection.dispatcher.totalStreamTime + queue.additionalStreamTime < 0) queue.additionalStreamTime = 0;
   if (queue.connection.dispatcher.totalStreamTime + queue.additionalStreamTime > queue.songs[0].duration*1000) return message.reply("you can't fast forward past the song's end");
   play(queue.songs[0], message, true); 
 };
