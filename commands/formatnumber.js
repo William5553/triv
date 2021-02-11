50668274a8f9cc24b2a7b2f47bf901ff665f0c8c
William Kang <random@williamk.ga>
2021-02-11 11:55:31 -0500

    Update formatnumber.js

--- commands/formatnumber.js
+++ commands/formatnumber.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => client.formatNumber(args.join(' '));
+exports.run = (client, message, args) => message.channel.send(client.formatNumber(args.join(' ')));
 
 exports.conf = {
   enabled: true,
