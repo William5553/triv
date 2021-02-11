3834ac60d185b3e75bebcc4cc0590cf277306742
William Kang <random@williamk.ga>
2021-02-11 12:41:24 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,3 +1,5 @@
+const ms = require('ms'),
+  db = require('quick.db');
 module.exports = async (client, message) => {
   if (message.author.bot) return;
   const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
@@ -17,10 +19,19 @@ module.exports = async (client, message) => {
   else if (client.aliases.has(command))
     cmd = client.commands.get(client.aliases.get(command));
   if (cmd) {
+    if (cmd.conf.cooldown && cmd.conf.cooldown > 0) {
+      const cooldownDb = await db.fetch(`cooldown_${cmd.help.name}_${message.author.id}`);
+      
+      if (cooldownDb !== null && cmd.conf.cooldown - (Date.now() - cooldownDb) > 0) {
+        const time = ms(cmd.conf.cooldown - (Date.now() - cooldownDb));
+        return message.channel.send(`Sorry you must wait **${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s** before using this command again!`);
+    }
+    }
     if (!message.guild) {
       if (!cmd.conf.guildOnly) {
         if (cmd.conf.permLevel >= 10 && message.author.id !== client.settings.owner_id)
           return message.reply("you don't have the perms for that");
+        db.set(`cooldown_${cmd.help.name}_${message.author.id}`, Date.now());
         return cmd.run(client, message, args, 4);
       } else if (cmd.conf.guildOnly)
         return message.reply('that command can only be used in a guild, get some friends.');
@@ -28,6 +39,7 @@ module.exports = async (client, message) => {
     const perms = client.elevation(message);
     if (perms < cmd.conf.permLevel)
       return message.reply("you don't have the perms for that");
-    cmd.run(client, message, args, perms);
+    db.set(`cooldown_${cmd.help.name}_${message.author.id}`, Date.now());
+    return cmd.run(client, message, args, perms);
   }
 };
