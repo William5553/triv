72bf63ae2f62948ece952e2266cfa9fe993339ab
William Kang <random@williamk.ga>
2021-02-11 01:30:02 -0500

    Update fastforward.js

--- commands/fastforward.js
+++ commands/fastforward.js
@@ -3,7 +3,7 @@ const { play } = require('../util/play'),
 exports.run = (client, message, args) => {
   const queue = client.queue.get(message.guild.id);
   if (!queue) return message.reply('nothing is playing');
-  if (isNaN(args[0]) return message.reply(`Usage: ${client.settings.prefix}${exports.help.usage}`);
+  if (isNaN(args[0])) return message.reply(`Usage: ${client.settings.prefix}${exports.help.usage}`);
   if (canModifyQueue(message.member) != true) return;
   queue.additionalTime = queue.additionalTime + tonumber(args[0])*1000;
   if (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime + queue.additionalStreamTime > song.duration*1000) return message.reply("you can't fast forward past the song's end");
