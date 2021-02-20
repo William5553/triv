ff449fc0fbf6808d739cfc054e88c4d73ff2ce38
William Kang <random@williamk.ga>
2021-02-20 18:26:05 -0500

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -4,14 +4,17 @@ exports.run = (client, message) => {
   if (!message.guild.voice.channel) return message.reply("i'm not in a voice channel moron");
   const queue = client.queue.get(message.guild.id);
 
-  if (canModifyQueue(message.member) != true) return;
+  if (canModifyQueue(message.member) != true) return message.delete();
   if (queue && queue.connection) {
     queue.songs = [];
     queue.connection.dispatcher.end();
     queue.textChannel.send(`${message.author} ⏹ stopped the music!`).catch(client.logger.error);
     if (queue.stream) queue.stream.destroy();
-  } else
+  } else {
+		if (!message.channel.permissionsFor(message.author).has('MOVE_MEMBERS') && message.guild.voice.connection.channel.members.size > 2)
+			return message.reply('you need the **MOVE MEMBERS** permission');
     message.member.voice.channel.leave();
+  }
 };
 
 exports.conf = {
