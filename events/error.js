69f4291fa9e661a5b52f2fe668a2d1081cef0b90
William K <random@williamk.ga>
2021-02-20 20:21:16 -0500

    Update error.js

--- events/error.js
+++ events/error.js
@@ -1,1 +1,1 @@
-module.exports = async (client, error) => client.logger.error(`An error event was sent by Discord.js:\n${JSON.stringify(error)}`);
+module.exports = (client, error) => client.logger.error(`An error event was sent by Discord.js:\n${JSON.stringify(error)}`);
