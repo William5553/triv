aa07546db3fffb6f7fc5b43add509c68fb5ff5e1
William K <random@williamk.ga>
2021-02-21 18:15:24 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,9 @@
 module.exports = client => {
   client.logger.log(`User: ${client.user.tag} Prefix: ${client.settings.prefix}`, 'ready');
-  if (client.guilds.cache.size === 1)
-    client.user.setActivity(`${client.settings.prefix}help | ${client.guilds.cache.size} guild`, { type: 'LISTENING' });
-  else
-    client.user.setActivity(`${client.settings.prefix}help | ${client.guilds.cache.size} guilds`, { type: 'LISTENING' });
+  setInterval(() => {
+    if (client.guilds.cache.size === 1)
+      client.user.setActivity(`${client.settings.prefix}help | ${client.guilds.cache.size} guild`, { type: 'LISTENING' });
+    else
+      client.user.setActivity(`${client.settings.prefix}help | ${client.guilds.cache.size} guilds`, { type: 'LISTENING' });
+  }, 60000);
 };
