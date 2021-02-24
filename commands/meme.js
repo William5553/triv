89cc52cbff6fb4ee11d590a8620c07ccaed52f6c
William Kang <random@williamk.ga>
2021-02-24 12:59:12 -0500

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require('discord.js'),
+  request = require('node-superfetch');
+
+exports.run = async (client, message, args) => {
+  try {
+    const { body } = await request.get('https://www.reddit.com/r/memes/random/.json');
+	
+	  const [list] = JSON.parse(body);
+	  const [post] = list.data.children;
+
+		message.channel.send(new MessageEmbed()
+      .setTitle(post.data.title)
+		  .setURL(`https://reddit.com${post.data.permalink}`)
+  		.setColor('RED')
+  		.setImage(post.data.url)
+	  	.setFooter(`👍 ${post.data.ups} 💬 ${post.data.num_comments}`)
+    );
+  } catch (err) {
+    return message.channel.send(new MessageEmbed()
+      .setColor('RED')
+      .setTimestamp()
+      .setTitle('Please report this on GitHub')
+      .setURL('https://github.com/william5553/triv/issues')
+      .setDescription(`**Stack Trace:**\n\`\`\`${err.stack}\`\`\``)
+      .addField('**Command:**', `${message.content}`)
+    );
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'meme',
+  description: 'Gets a random meme from r/memes',
+  usage: 'meme'
+};
