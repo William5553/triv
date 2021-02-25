3848fedd799f2547f1c087ef9a74fd17275af914
William Kang <random@williamk.ga>
2021-02-25 12:30:45 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async client => {
-  client.logger.log(`\nUser: ${client.user.tag}\nPrefix: ${client.settings.prefix}\nServing ${client.users.cache.size} users in ${client.guilds.cache.size} server${client.guilds.cache.size === 1 ? '' : 's'}`, 'ready');
+  client.logger.log(`User: ${client.user.tag} | Prefix: ${client.settings.prefix} | Serving ${client.users.cache.size} users in ${client.guilds.cache.size} server${client.guilds.cache.size === 1 ? '' : 's'}`, 'ready');
   client.user.setActivity(`${client.settings.prefix}help | ${client.guilds.cache.size} guild${client.guilds.cache.size === 1 ? '' : 's'}`, { type: 'LISTENING' });
 
   client.application = await client.fetchApplication();
