c6f64549ab5f550f0847a9a67cbb594f0187e052
Glitch (triv) <none>
2021-02-26 17:00:48 +0000

    🌥👡 Updated with Glitch

--- commands/guilds.js
+++ commands/guilds.js
@@ -1,14 +1,15 @@
 const { MessageEmbed } = require('discord.js');
+
 exports.run = async (client, message) => {
   const link = await client.generateInvite({ permissions: 2146958591 }).catch(message.channel.send);
-    message.channel.send(new MessageEmbed()
-      .setColor(0x00ff5c)
-      .setAuthor(client.user.username, client.user.avatarURL())
-      .setTitle(`**${client.guilds.cache.size} guild(s)**`)
-      .setDescription(`${client.guilds.cache.map(g => `${g.name} - ${g.id}`).join('\n')}`)
-      .setURL(client.application.botPublic ? link : 'https://github.com/william5553/triv')
-      .setTimestamp()
-    );
+  message.channel.send(new MessageEmbed()
+    .setColor(0x00ff5c)
+    .setAuthor(client.user.username, client.user.avatarURL())
+    .setTitle(`**${client.guilds.cache.size} guild(s)**`)
+    .setDescription(`${client.guilds.cache.map(g => `${g.name} - ${g.id}`).join('\n')}`)
+    .setURL(client.application.botPublic ? link : 'https://github.com/william5553/triv')
+    .setTimestamp()
+  );
 };
 
 exports.conf = {
