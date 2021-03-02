bcfc6eacdd88c09b5a6ec9433e944e6fcf28cb9b
William Kang <random@williamk.ga>
2021-03-02 13:34:01 -0500

    Update play.js

--- util/play.js
+++ util/play.js
@@ -102,12 +102,12 @@ module.exports = {
       playingMessage = await queue.textChannel.send(new MessageEmbed()
         .setTitle(song.title)
         .setURL(song.url)
-        .setColor('RED')
+        .setColor('#FF0000')
         .setThumbnail(song.thumbnail.url)
         .setTimestamp()
-        .setDescription(`${seekTime >= 1 ? `Starting at ${new Date(seekTime).toISOString().substr(11, 8)}` : ''}\nVideo length: ${song.duration == 0 ? ' ◉ LIVE' : new Date(song.duration*1000).toISOString().substr(11, 8)}`)
+        .setDescription(`${seekTime >= 1 ? `Starting at ${new Date(seekTime).toISOString().substr(11, 8)}` : ''}`)
         .setAuthor(song.channel.name, song.channel.thumbnails[song.channel.thumbnails.length - 1].url, song.channel.user_url)
-        .setFooter(`Published on ${song.publishDate}`)
+        .setFooter(`Length: ${song.duration == 0 ? ' ◉ LIVE' : new Date(song.duration*1000).toISOString().substr(11, 8)} | Published on ${song.publishDate}`)
       );
       await playingMessage.react('⏭');
       await playingMessage.react('⏯');
