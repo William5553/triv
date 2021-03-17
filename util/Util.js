0af1706039f87756bc6013c9906fa4637878c8e5
William Kang <random@williamk.ga>
2021-03-17 17:18:24 -0400

    Update Util.js

--- util/Util.js
+++ util/Util.js
@@ -67,12 +67,12 @@ module.exports = class Util {
   }
 
   static parseUser(message, member) {
-    if (message.client.owners.includes(member.user.id))
-      return message.reply('no!');
     if (member.user === message.client.user)
       return message.reply('you are an idiot');
     if (member.user === message.author)
       return message.reply("you can't do that to yourself, why did you try? you are an idiot.");
+    if (message.client.owners.includes(member.user.id))
+      return message.reply('no!');
     if (member && member.roles && member.roles.highest.position >= message.member.roles.highest.position)
       return message.reply('that member is higher or equal to you. L');
     if (member && member.roles && member.roles.highest.position >= message.guild.me.roles.highest.position)
