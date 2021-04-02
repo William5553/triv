d3d12f568b2217adb5e54067bc526a3f71831349
William K <random@williamk.ga>
2021-04-02 19:59:14 -0400

    Update profilepic.js

--- commands/profilepic.js
+++ commands/profilepic.js
@@ -1,6 +1,20 @@
+const { MessageEmbed } = require('discord.js');
+
 exports.run = (client, message, args) => {
-  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args[0].toLowerCase()) || message.member;
-  message.channel.send(member.user.displayAvatarURL({size: 4096, dynamic: true}));
+  try {
+    if (args.length < 1) return message.reply(`usage: ${client.getPrefix()}${exports.help.usage}`);
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args.join(' ')) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLowerCase());
+    message.channel.send(member.user.displayAvatarURL({size: 4096, dynamic: true}));
+  } catch (err) {
+    return message.channel.send(new MessageEmbed()
+      .setColor('#FF0000')
+      .setTimestamp()
+      .setTitle('Please report this on GitHub')
+      .setURL('https://github.com/william5553/triv/issues')
+      .setDescription(`**Stack Trace:**\n\`\`\`${err.stack}\`\`\``)
+      .addField('**Command:**', `${message.content}`)
+    );
+  }
 };
 
 exports.conf = {
