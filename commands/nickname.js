61202fdb31e40cf24fd7bd68bc04ed4cb0e3b845
William K <random@williamk.ga>
2021-04-02 19:38:54 -0400

    Update nickname.js

--- commands/nickname.js
+++ commands/nickname.js
@@ -1,29 +1,41 @@
+const { MessageEmbed } = require('discord.js');
 const { parseUser } = require('../util/Util');
 
 exports.run = (client, message, args) => {
-  if (parseUser(message, member) !== true) return;
-  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args[0].toLowerCase());
-  const oldNick = member.displayName;
-  const newNick = args.splice(1).join(' ');
-  member.setNickname(newNick);
-  message.channel.send(`Updated ${member.toString()}'s nickname from ${oldNick} to ${newNick}`);
+  try {
+    if (parseUser(message, member) !== true) return;
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args[0].toLowerCase());
+    const oldNick = member.displayName;
+    const newNick = args.splice(1).join(' ');
+    member.setNickname(newNick);
+    message.channel.send(`Updated ${member.toString()}'s nickname from ${oldNick} to ${newNick}`);
 
-  client.infractions.ensure(message.guild.id, { [member.id]: [] });
-  client.infractions.push(message.guild.id, {
-    type: 'Update Nickname',
-    timestamp: Date.now(),
-    mod: message.author.id,
-    additional: [
-      {
-        title: 'Old Nickname',
-        body: oldNick
-      },
-      {
-        title: 'New Nickname',
-        body: newNick
-      }
-    ]
-  }, member.id);
+    client.infractions.ensure(message.guild.id, { [member.id]: [] });
+    client.infractions.push(message.guild.id, {
+      type: 'Update Nickname',
+      timestamp: Date.now(),
+      mod: message.author.id,
+      additional: [
+        {
+          title: 'Old Nickname',
+          body: oldNick
+        },
+        {
+          title: 'New Nickname',
+          body: newNick
+        }
+      ]
+    }, member.id);
+  } catch (err) {
+    return message.channel.send(new MessageEmbed()
+      .setColor('#FF0000')
+      .setTimestamp()
+      .setTitle('Please report this on GitHub')
+      .setURL('https://github.com/william5553/triv/issues')
+      .setDescription(`**Stack Trace:**\n\`\`\`${err.stack}\`\`\``)
+      .addField('**Command:**', `${message.content}`)
+    );
+  }
 };
 
 exports.conf = {
