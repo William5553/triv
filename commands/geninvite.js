bd4f283ff86658640441d91d6fb269812a1c52a2
William K <random@williamk.ga>
2021-04-03 18:18:35 -0400

    Create geninvite.js

---
+++ commands/geninvite.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require('discord.js'); 
+
+exports.run = async (client, message, args) => {
+  try {
+    if (isNaN(args[0])) return message.reply('please provide a valid guild ID.');
+    const guild = client.guilds.resolve(args[0]);
+    if (!guild) return message.channel.send('Unable to find server, please check the provided ID');
+    const inv = await guild.channels.cache.first().createInvite({ maxAge: 0, maxUses: 0 });
+    message.channel.send(inv.url);
+  } catch (err) {
+    return message.channel.send(new MessageEmbed()
+      .setColor('#FF0000')
+      .setTimestamp()
+      .setTitle('Please report this on GitHub')
+      .setURL('https://github.com/william5553/triv/issues')
+      .setDescription(`**Stack Trace:**\n\`\`\`${err.stack}\`\`\``)
+      .addField('**Command:**', `${message.content}`)
+    );
+  }
+};
+  
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 10
+};
+  
+exports.help = {
+  name: 'geninvite',
+  description: 'Makes an invite for the guild specified and sends it',
+  usage: 'geninvite [guild id]'
+};
+  
