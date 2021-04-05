c3f04ad4a59d8d8cf30dc51a62c8d147a2cc8ab2
William K <random@williamk.ga>
2021-04-05 13:18:22 -0400

    Update messageUpdate.js

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,7 +2,10 @@ const { MessageEmbed } = require('discord.js');
 module.exports = (client, oldMessage, newMessage) => {
   if (!newMessage.content || !oldMessage.content || !newMessage.guild) return;
   
-
+  const findLogs = newMessage.guild.channels.cache.find(channel => channel.name === 'bot-logs') || newMessage.guild.channels.cache.find(channel => channel.name === 'logs');
+  if (findLogs) {
+    client.settings.set(newMessage.guild.id, findLogs.id, 'logsID');
+  }
   
   if (client.settings.get(newMessage.guild.id).logsID) {
     const logs = newMessage.guild.channels.resolve(client.settings.get(newMessage.guild.id).logsID);
