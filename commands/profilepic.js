9a1544e0863870c19c1f0b16bc9834d92e462f44
William K <random@williamk.ga>
2021-04-08 16:46:25 -0400

    Update profilepic.js

--- commands/profilepic.js
+++ commands/profilepic.js
@@ -3,9 +3,9 @@ const possibleFormats = ['webp', 'png', 'jpg', 'jpeg', 'gif'];
 
 exports.run = (client, message, args) => {
   try {
-    if (args.length < 2) return message.reply(`usage: ${client.getPrefix()}${exports.help.usage} (formats: ${possibleFormats.join(', ')})`);
+    if (args.length < 2) return message.reply(`usage: ${client.getPrefix(message)}${exports.help.usage} (formats: ${possibleFormats.join(', ')})`);
     if (!possibleFormats.includes(args[0])) return message.reply(`Invalid format, valid formats: ${possibleFormats}`);
-    const member = message.mentions.members.first() || message.guild.members.cache.get(args.join(' ')) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLowerCase());
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args.splice(1).join(' ')) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.splice(1).join(' ').toLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.splice(1).join(' ').toLowerCase());
     if (!member) return message.reply(`usage: ${client.getPrefix()}${exports.help.usage}`);
     message.channel.send(member.user.displayAvatarURL({ size: 4096, dynamic: true, format: args[0] }));
   } catch (err) {
