7c053a3439de88a1bbb556273bb62eb2319c9f1c
William K <random@williamk.ga>
2021-04-09 15:37:02 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,38 +1,43 @@
 const ms = require('ms');
+const { MessageEmbed } = require('discord.js');
 
 exports.run = (client, message, args, perms) => {
-  if (!args[0]) {
-    const longest = Array.from(client.commands.keys()).reduce((long, str) => Math.max(long, str.length), 0);
-    const fonk = client.commands.map(c => {
-      if (perms < c.conf.permLevel)
-        return null;
-      else
-        return `${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`;
-    })
-      .filter(a => a !== null)
-      .join('\n');
-    const m =`= Command List =\n\n[Use help <commandname> for details]\n\n${fonk}`;
-    for (let i = 0; i * 1980 <= m.length; i++) {
-      message.author
-        .send(m.substring(i * 1980, i * 1980 + 1980), { code: 'asciidoc' })
-        .catch(client.logger.error);
+  try {
+    if (!args[0]) {
+      const longest = Array.from(client.commands.keys()).reduce((long, str) => Math.max(long, str.length), 0);
+      const fonk = client.commands.map(c => {
+        return perms < c.conf.permLevel ? null : `${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`;
+      })
+        .filter(a => a !== null)
+        .join('\n');
+      const m =`= Command List =\n\n[Use help <commandname> for details]\n\n${fonk}`;
+      message.author.send(m, { code: 'asciidoc', split: true }).catch(message.channel.send);
+      if (message.guild) message.channel.send('Help sent to your DMs! :mailbox_with_mail:');
+    } else {
+      let command;
+      if (client.commands.has(args[0]))
+        command = client.commands.get(args[0]);
+      else if (client.aliases.has(args[0]))
+        command = client.commands.get(client.aliases.get(args[0]));
+      if (!command) return message.channel.send(`${args[0]} is not a valid command`);
+      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage    :: ${command.help.usage}\naliases  :: ${command.conf.aliases.join(', ') || 'none'}\nexample  :: ${command.help.example || 'no example provided'}\ncooldown :: ${command.conf.cooldown ? ms(command.conf.cooldown) : '0s'}`, { code: 'asciidoc' });
     }
-    if (message.guild) message.channel.send('Help sent to your DMs! :mailbox_with_mail:');
-  } else {
-    let command;
-    if (client.commands.has(args[0]))
-      command = client.commands.get(args[0]);
-    else if (client.aliases.has(args[0]))
-      command = client.commands.get(client.aliases.get(args[0]));
-    if (!command) return message.channel.send(`${args[0]} is not a valid command`);
-    message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage    :: ${command.help.usage}\naliases  :: ${command.conf.aliases.join(', ') || 'none'}\nexample  :: ${command.help.example || 'no example provided'}\ncooldown :: ${command.conf.cooldown ? ms(command.conf.cooldown) : '0s'}`, { code: 'asciidoc' });
+  } catch (err) {
+    return message.channel.send(new MessageEmbed()
+      .setColor('#FF0000')
+      .setTimestamp()
+      .setTitle('Please report this on GitHub')
+      .setURL('https://github.com/william5553/triv/issues')
+      .setDescription(`**Stack Trace:**\n\`\`\`${err.stack}\`\`\``)
+      .addField('**Command:**', `${message.content}`)
+    );
   }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['commands', 'usage'],
+  aliases: ['commands', 'usage', 'halp'],
   permLevel: 0,
   cooldown: 1500
 };
