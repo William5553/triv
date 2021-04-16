ceaf5bc01294a250793d7c38aeddc98855b59ef8
William K <random@williamk.ga>
2021-04-15 20:10:23 -0400

    .

--- commands/gautofill.js
+++ commands/gautofill.js
@@ -28,7 +28,7 @@ exports.run = async (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['gauto'],
+  aliases: ['gauto', 'autofill'],
   permLevel: 0
 };
 
