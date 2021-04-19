88a1cec1249693195d165ab9d4f5fd3f375e2bd2
William Kang <random@williamk.ga>
2021-04-19 02:13:57 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,10 @@
 module.exports = async client => {
   client.logger.log(`User: ${client.user.tag} | Prefix: ${process.env.prefix} | ${client.commands.array().length} commands | Serving ${client.users.cache.size} users in ${client.guilds.cache.size} server${client.guilds.cache.size === 1 ? '' : 's'}`, 'ready');
-  client.user.setActivity(`${process.env.prefix}help | ${client.commands.array().length} cmd${client.commands.array().length === 1 ? '' : 's'}`, { type: 'LISTENING' });
+  client.user.setActivity(`${process.env.prefix}help | ${client.commands.size} cmd${client.commands.array().length === 1 ? '' : 's'}`, { type: 'LISTENING' });
 
   client.application = await client.fetchApplication();
   if (client.owners.length < 1) client.application.team ? client.owners.push(...client.application.team.members.keys()) : client.owners.push(client.application.owner.id);
   setInterval(async () => {
-    client.user.setActivity(`${process.env.prefix}help | ${client.commands.array().length} cmd${client.commands.array().length === 1 ? '' : 's'}`, { type: 'LISTENING' });
     client.owners = [];
     client.application = await client.fetchApplication();
     client.application.team ? client.owners.push(...client.application.team.members.keys()) : client.owners.push(client.application.owner.id);
