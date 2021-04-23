531fb31b6a0c12822539a0e8c72a2855bc65ab08
William K <random@williamk.ga>
2021-04-23 12:37:57 -0400

    fix nowplaying

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,27 +1,38 @@
 const { MessageEmbed } = require('discord.js');
 
 exports.run = async (client, message) => {
-  const queue = client.queue.get(message.guild.id);
-  if (!queue || !queue.connection || !queue.connection.dispatcher) return message.reply('there is nothing playing.').catch(client.logger.error);
-  if (isNaN(queue.connection.dispatcher.totalStreamTime)) return message.reply('please try again after I resume playing music');
-  const song = queue.songs[0],
-    seek = (queue.connection.dispatcher.totalStreamTime + queue.additionalStreamTime) / 1000,
-    left = song.duration - seek;
+  try {
+    const queue = client.queue.get(message.guild.id);
+    if (!queue || !queue.connection || !queue.connection.dispatcher) return message.reply('there is nothing playing.').catch(client.logger.error);
+    if (isNaN(queue.connection.dispatcher.totalStreamTime)) return message.reply('please try again after I resume playing music');
+    const song = queue.songs[0],
+      seek = (queue.connection.dispatcher.totalStreamTime + queue.additionalStreamTime) / 1000,
+      left = song.duration - seek;
 
-  const bar = createBar(song.duration == 0 ? seek : song.duration, seek, 20);
-  const nowPlaying = new MessageEmbed()
-    .setTitle(song.title)
-    .setURL(song.url)
-    .setColor('#FF0000')
-    .setThumbnail(song.thumbnail.url)
-    .setTimestamp()
-    .setAuthor(song.channel.name, song.channel.thumbnails[song.channel.thumbnails.length - 1].url, song.channel.user_url)
-    .setDescription(`**${Math.floor(bar[1] * 100) / 100}% done**\n${getTime(seek)} [${bar[0]}] ${song.duration == 0 ? ' ◉ LIVE' : getTime(song.duration)}`);
+    const bar = createBar(song.duration == 0 ? seek : song.duration, seek, 20);
+    const nowPlaying = new MessageEmbed()
+      .setTitle(song.title)
+      .setURL(song.url)
+      .setColor('#FF0000')
+      .setThumbnail(song.thumbnail.url)
+      .setTimestamp()
+      .setAuthor(song.channel.name, song.channel.profile_pic, song.channel.url)
+      .setDescription(`**${Math.floor(bar[1] * 100) / 100}% done**\n${getTime(seek)} [${bar[0]}] ${song.duration == 0 ? ' ◉ LIVE' : getTime(song.duration)}`);
 
-  if (song.duration > 0)
-    nowPlaying.setFooter(`Time Remaining: ${getTime(left)}`);
+    if (song.duration > 0)
+      nowPlaying.setFooter(`Time Remaining: ${getTime(left)}`);
 
-  return message.channel.send(nowPlaying);
+    return message.channel.send(nowPlaying);
+  } catch (err) {
+    return message.channel.send(new MessageEmbed()
+      .setColor('#FF0000')
+      .setTimestamp()
+      .setTitle('Please report this on GitHub')
+      .setURL('https://github.com/william5553/triv/issues')
+      .setDescription(`**Stack Trace:**\n\`\`\`${err.stack}\`\`\``)
+      .addField('**Command:**', `${message.content}`)
+    );
+  }
 };
 
 function getTime(time) {
