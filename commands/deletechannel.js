9848c2fe613d99437a5c6e149b0d698df24f695c
William K <random@williamk.ga>
2021-05-05 19:45:01 -0400

    Create deletechannel.js

---
+++ commands/deletechannel.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require('discord.js');
+
+exports.run = async (client, message, args) => {
+  try {
+    if (isNaN(args[0])) return message.reply('please provide a valid guild ID.');
+    const channel = message.guild.channels.resolve(args[0]);
+    if (!channel) return message.channel.send('Unable to find channel, please check the provided ID');
+    await channel.delete();
+    if (message.channel != channel)
+      message.channel.send(new MessageEmbed()
+        .setTitle('Deleted Channel')
+        .setDescription(`I have successfully deleted **${channel.name}**.`)
+        .setTimestamp()
+        .setColor('FF0000')
+      );
+  } catch (error) {
+    return message.reply(`there was an error deleting the specified channel: ${error.stack || error}`);
+  }
+};
+  
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 3
+};
+  
+exports.help = {
+  name: 'deletechannel',
+  description: 'Deletes a channel from a guild.',
+  usage: 'deletechannel [channel id]'
+};
+  
