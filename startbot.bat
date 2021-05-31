77d70f13f07ad370dae763a19a0838e139770a88
William Kang <contact@williamk.ga>
2021-05-31 12:35:50 -0400

    .

---
+++ startbot.bat
@@ -0,0 +1,8 @@
+@echo off
+title Triv - github.com/William5553/triv
+echo Starting..
+
+:main
+node index.js
+echo Restarting...
+goto main
---
+++ startbot.sh
@@ -0,0 +1,7 @@
+# /bin/sh
+while true; do
+    echo "Starting Bot ..."
+    node index.js
+    echo "Bot crashed ... Restarting in 5 seconds..."
+    sleep 5
+done
