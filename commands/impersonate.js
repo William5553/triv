7cd99ce228f09fd267b320b420cc41dab902a34a
William Kang <contact@williamk.ga>
2021-06-07 10:14:20 -0400

    Update impersonate.js

--- commands/impersonate.js
+++ commands/impersonate.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args) => {
   if (args.length < 2) return message.reply(`usage: ${client.getPrefix(message)}${exports.help.usage}`);
   message.delete();
-  const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args[0].toLowerCase());
+  const member = message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args[0].toLowerCase()) || message.mentions.members.first();
   const webhook = await message.channel.createWebhook(member.displayName, { avatar: member.user.displayAvatarURL({ dynamic: true }) });
   await webhook.send(args.slice(1).join(' '));
   webhook.delete();
