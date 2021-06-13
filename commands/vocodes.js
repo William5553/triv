bd68d17a6590de09714ded53fb59b7cc6086151f
William Kang <contact@williamk.ga>
2021-06-13 17:02:04 -0400

    Update vocodes.js

--- commands/vocodes.js
+++ commands/vocodes.js
@@ -1,12 +1,13 @@
 const request = require('node-superfetch');
 const { Readable } = require('stream');
 const { MessageEmbed, Message, Permissions } = require('discord.js');
+const { createAudioPlayer, createAudioResource, getVoiceConnection, StreamType, AudioPlayerStatus } = require('@discordjs/voice');
 const voices = require('../assets/vocodes.json');
 
 exports.run = async (client, message, args) => {
   const queue = client.queue.get(message.guild.id);
   if (queue) return message.reply("there's currently music playing");
-  let voice = args[0];
+  let connection, voice = args[0];
   const text = args.slice(1).join(' ');
   if (!voice || !Object.keys(voices).includes(voice.toLowerCase()))
     return message.channel.send(`Possible voices: ${Object.keys(voices).join(', ')}`);
@@ -16,7 +17,7 @@ exports.run = async (client, message, args) => {
     return message.reply('keep the message under 500 characters man');
   voice = voices[voice.toLowerCase()];
   if (!getVoiceConnection(message.guild.id)) {
-    const connection = await client.commands.get('join').run(client, message);
+    connection = await client.commands.get('join').run(client, message);
     if (connection instanceof Message) return;
   } else if (message.member.voice.channelID !== message.guild.me.voice.channelID)
     return message.reply("I'm already in a voice channel");
@@ -27,11 +28,29 @@ exports.run = async (client, message, args) => {
         speaker: voice,
         text
       });
-    message.guild.me.voice.connection
-      .play(Readable.from([Buffer.from(body.audio_base64, 'base64')]))
-      .on('finish', () => message.member.voice.channel.leave())
-      .on('error', err => client.logger.error(err));
-    if (message.channel.permissionsFor(client.user).has([Permissions.FLAGS.ADD_REACTIONS, Permissions.FLAGS.READ_MESSAGE_HISTORY]))
+    if (!connection)
+      connection = getVoiceConnection(message.guild.id);
+    const player = createAudioPlayer();
+    player.on('error', error => {
+      client.logger.error(`An audio player encountered an error: ${error.stack || error}`);
+      message.channel.send({embeds: [
+        new MessageEmbed()
+          .setColor('#FF0000')
+          .setTimestamp()
+          .setTitle('Please report this on GitHub')
+          .setURL('https://github.com/william5553/triv/issues')
+          .setDescription(`**The audio player encountered an error.\nStack Trace:**\n\`\`\`${error.stack || error}\`\`\``)
+          .addField('**Command:**', `${message.content}`)
+      ]});
+    });
+    player.on(AudioPlayerStatus.Idle, () => {
+      connection.destroy();
+      player.stop();
+    });
+    const resource = createAudioResource(Readable.from([Buffer.from(body.audio_base64, 'base64')]), { inputType: StreamType.Arbitrary });
+    player.play(resource);
+    connection.subscribe(player);
+    if (message.channel.permissionsFor(client.user).has(['ADD_REACTIONS', 'READ_MESSAGE_HISTORY']))
       message.react('🔉');
     return null;
   } catch (err) {
