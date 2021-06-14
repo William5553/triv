469726b4f1c69c39544c71b00274ca0f21d3beaf
William Kang <contact@williamk.ga>
2021-06-14 18:18:57 -0400

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -22,7 +22,7 @@ module.exports = async (client, message) => {
       // Fetch a couple audit logs than just one as new entries could've been added right after this event was emitted.
       const fetchedLogs = await message.guild.fetchAuditLogs({ limit: 8, type: 'MESSAGE_DELETE', user: message.author });
 
-      client.logger.log(JSON.stringify(fetchedLogs));
+      client.logger.log(JSON.stringify(fetchedLogs.entries));
       // Small filter function to make use of the little discord provides to narrow down the correct audit entry.
       // Ignore entries that are older than 20 seconds to reduce false positives.
       const auditEntry = fetchedLogs.entries.find(a => a.target.id === message.author?.id && a.extra.channel.id === message.channel.id && Date.now() - a.createdTimestamp < 20000);
