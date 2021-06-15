cf22dbeebff61389f0761020f19e0b18c47d9022
William Kang <random@williamk.ga>
2021-06-14 23:59:45 -0400

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -5,7 +5,8 @@ const Genius = require('genius-lyrics');
 exports.run = async (client, message, args) => {
   const GClient = new Genius.SongsClient(process.env.genius_api_key || '');
   let query, queue;
-  if (message.guild) queue = client.queue.get(message.guild.id);
+  if (message.guild)
+    queue = client.queue.get(message.guild.id);
   if (args && args.length >= 1)
     query = args.join(' ');
   else if (queue && queue.songs)
@@ -45,11 +46,12 @@ exports.run = async (client, message, args) => {
 
   for (let i = 0; i * 1850 <= lyrics.length; i++) {
     embeds.push(new MessageEmbed()
-      .setTitle('Lyrics - ' + emtitle)
+      .setTitle(`Lyrics - ${emtitle}`)
       .setDescription(lyrics.substr(i * 1850, i * 1850 + 1850))
-      .setColor('#F8AA2A'));
+      .setColor('#F8AA2A')
+    );
   }
-  message.channel.send(embeds);
+  message.channel.send({embeds});
 };
 
 exports.conf = {
