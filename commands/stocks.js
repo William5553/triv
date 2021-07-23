5762b56d73e7564acbc71713d5e6b1564b24b4b7
William Kang <contact@williamk.ga>
2021-07-23 18:29:30 -0400

    .

--- commands/stocks.js
+++ commands/stocks.js
@@ -4,44 +4,47 @@ const { MessageEmbed } = require('discord.js');
 
 exports.run = async (client, message, args) => {
   try {
-    if (args.length < 1) return message.channel.send(`${client.getPrefix(message)}${exports.help.usage}`);
+    if (!process.env.alphavantage_key) return message.reply('The bot owner has not set up this command yet.');
+    if (args.length < 1) return message.reply(`Usage: ${client.getPrefix(message)}${exports.help.usage}`);
     const query = args.join(' ');
     const company = await search(query);
     if (!company) return message.channel.send('Could not find any results.');
-    const stocks = await fetchStocks(company.symbol, process.env.alphavantage_key);
+    const stocks = await fetchStocks(company.symbol);
     if (!stocks) return message.channel.send('Could not find any results.');
-    return message.channel.send({embeds: [new MessageEmbed()
-      .setTitle(`Stocks for ${company.name} (${stocks.symbol.toUpperCase()})`)
-      .setColor(0x9797FF)
-      .setFooter('Last Updated')
-      .setTimestamp(stocks.lastRefresh)
-      .addField('❯ Open', `$${formatNumber(stocks.open)}`, true)
-      .addField('❯ Close', `$${formatNumber(stocks.close)}`, true)
-      .addField('❯ Volume', formatNumber(stocks.volume), true)
-      .addField('❯ High', `$${formatNumber(stocks.high)}`, true)
-      .addField('❯ Low', `$${formatNumber(stocks.low)}`, true)
-      .addField('\u200B', '\u200B', true)
+    return message.reply({embeds: [
+      new MessageEmbed()
+        .setTitle(`Stocks for ${company.name} (${stocks.symbol.toUpperCase()})`)
+        .setColor(0x9797FF)
+        .setFooter('Last Updated')
+        .setTimestamp(stocks.lastRefresh)
+        .addField('❯ Open', `$${formatNumber(stocks.open)}`, true)
+        .addField('❯ Close', `$${formatNumber(stocks.close)}`, true)
+        .addField('❯ Volume', formatNumber(stocks.volume), true)
+        .addField('❯ High', `$${formatNumber(stocks.high)}`, true)
+        .addField('❯ Low', `$${formatNumber(stocks.low)}`, true)
+        .addField('\u200B', '\u200B', true)
     ]});
   } catch (err) {
-    return message.channel.send({embeds: [new MessageEmbed()
-      .setColor('#FF0000')
-      .setTimestamp()
-      .setTitle('Please report this on GitHub')
-      .setURL('https://github.com/william5553/triv/issues')
-      .setDescription(`**Stack Trace:**\n\`\`\`${err.stack || err}\`\`\``)
-      .addField('**Command:**', message.content)
+    return message.channel.send({embeds: [
+      new MessageEmbed()
+        .setColor('#FF0000')
+        .setTimestamp()
+        .setTitle('Please report this on GitHub')
+        .setURL('https://github.com/william5553/triv/issues')
+        .setDescription(`**Stack Trace:**\n\`\`\`${err.stack || err}\`\`\``)
+        .addField('**Command:**', message.content)
     ]});
   }
 };
 
-async function fetchStocks(symbol, key) {
+async function fetchStocks(symbol) {
   const { body } = await request
     .get('https://www.alphavantage.co/query')
     .query({
       function: 'TIME_SERIES_INTRADAY',
       symbol,
       interval: '1min',
-      apikey: key
+      apikey: process.env.alphavantage_key
     });
   if (body['Error Message'] || !body['Time Series (1min)']) return null;
   const data = Object.values(body['Time Series (1min)'])[0];
