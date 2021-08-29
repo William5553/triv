ca72dd6489b124929bab3c050f64d82ae476f789
William Kang <contact@williamk.ga>
2021-08-28 23:28:25 -0400

    .

--- commands/img.js
+++ commands/img.js
@@ -6,26 +6,28 @@ exports.run = async (client, message, args) => {
     if (!process.env.imgur_key) return message.reply('The bot owner has not set up this command yet.');
     if (!args) return message.channel.send(`${client.getPrefix(message)}${exports.help.usage}`);
     const { body } = await fetch
-      .get(`https://api.imgur.com/3/gallery/search/top/year?q=${args.join(' ')}`)
+      .get(`https://api.imgur.com/3/gallery/search/top/all?q=${args.join(' ')}`)
       .set({ Authorization: `Client-ID ${process.env.imgur_key}` });
     const images = body.data.filter(image => image.images && !image.images[0].type.includes('video') && (message.channel.nsfw ? true : !image.nsfw));
     if (!images.length) return message.channel.send('Could not find any results.');
     const image = images.random();
-    message.channel.send({embeds: [new MessageEmbed()
-      .setTitle(image.title)
-      .setURL(image.link)
-      .setColor('GREEN')
-      .setImage(image.images[0].link)
-      .setFooter(`👁️ ${image.views} | 👍 ${image.ups} | 👎 ${image.downs}`)
+    message.channel.send({embeds: [
+      new MessageEmbed()
+        .setTitle(image.title)
+        .setURL(image.link)
+        .setColor('GREEN')
+        .setImage(image.images[0].link)
+        .setFooter(`👁️ ${image.views} | 👍 ${image.ups} | 👎 ${image.downs}`)
     ]});
   } catch (err) {
-    return message.channel.send({embeds: [new MessageEmbed()
-      .setColor('#FF0000')
-      .setTimestamp()
-      .setTitle('Please report this on GitHub')
-      .setURL('https://github.com/william5553/triv/issues')
-      .setDescription(`**Stack Trace:**\n\`\`\`${err.stack || err}\`\`\``)
-      .addField('**Command:**', message.content)
+    return message.channel.send({embeds: [
+      new MessageEmbed()
+        .setColor('#FF0000')
+        .setTimestamp()
+        .setTitle('Please report this on GitHub')
+        .setURL('https://github.com/william5553/triv/issues')
+        .setDescription(`**Stack Trace:**\n\`\`\`${err.stack || err}\`\`\``)
+        .addField('**Command:**', message.content)
     ]});
   }
 };
