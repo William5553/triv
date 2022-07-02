c8f73c54e2836c2e1bc14cd7eb9bf95aaef65c0b
William Kang <contact@williamk.ga>
2022-07-02 19:15:05 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const process = require('node:process');
 const { setInterval } = require('node:timers');
 
 module.exports = async client => {
-  client.logger.log(`User: ${client.user.tag} | Prefix: ${process.env.prefix} | ${client.commands.size} commands | Serving ${client.users.cache.size} users in ${client.guilds.cache.size} server${client.guilds.cache.size === 1 ? '' : 's'} | Node ${process.version} | Discord.js ${version}`, 'ready');
+  client.logger.log(`User: ${client.user.tag} (${client.user.id}) | Prefix: ${process.env.prefix} | ${client.commands.size} commands | Serving ${client.users.cache.size} users in ${client.guilds.cache.size} server${client.guilds.cache.size === 1 ? '' : 's'} | Node ${process.version} | Discord.js ${version}`, 'ready');
   client.user.setActivity(`${process.env.prefix}help | ${client.commands.size} cmd${client.commands.size === 1 ? '' : 's'}`, { type: 'LISTENING' });
 
   client.application = await client.application?.fetch();
