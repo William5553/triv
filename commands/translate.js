f5506ab4613a0e5008dc9bb37935a8a214371baa
William Kang <contact@williamk.ga>
2022-07-02 20:17:32 -0400

    translate

--- commands/translate.js
+++ commands/translate.js
@@ -1,19 +1,33 @@
 const translate = require('@william5553/translate-google-api');
+const { MessageEmbed } = require('discord.js');
 const { clean, getCode } = require('../util/Util');
 
 exports.run = async (client, message, args) => {
-  if (args.length < 3) return message.reply(`Wrong format: An example would be \`${client.getPrefix(message)}${exports.help.name} en fr english text here\` which would translate \`english text here\` into french`);
-  const text = args.slice(2).join(' ');
+  try {
+    if (args.length < 3) return message.reply(`Wrong format: An example would be \`${client.getPrefix(message)}${exports.help.name} en fr english text here\` which would translate \`english text here\` into french`);
+    const text = args.slice(2).join(' ');
 
-  const a1 = getCode(args[0].toProperCase());
-  const a2 = getCode(args[1].toProperCase());
+    const a1 = getCode(args[0].toProperCase());
+    const a2 = getCode(args[1].toProperCase());
 
-  if (!getCode(a1)) return message.reply(`${args[0]} isn't a supported language`);
-  if (!getCode(a2)) return message.reply(`${args[1]} isn't a supported language`);
+    if (!getCode(a1)) return message.reply(`${args[0]} isn't a supported language`);
+    if (!getCode(a2)) return message.reply(`${args[1]} isn't a supported language`);
 
-  let t = await translate(text, { from: a1, to: a2 }).catch(error => message.reply(error.toString()));
-  t = await clean(t[0]);
-  if (t) message.reply(t);
+    let t = await translate(text, { from: a1, to: a2 });
+    t = await clean(t[0]);
+    if (t) message.reply(t);
+  } catch (error) {
+    client.logger.error(error.stack ?? error);
+    return message.channel.send({embeds: [
+      new MessageEmbed()
+        .setColor('#FF0000')
+        .setTimestamp()
+        .setTitle('Please report this on GitHub')
+        .setURL('https://github.com/william5553/triv/issues')
+        .setDescription(`**Stack Trace:**\n\`\`\`${error.stack ?? error}\`\`\``)
+        .addField('**Command:**', message.content)
+    ]});
+  }
 };
 
 exports.conf = {
