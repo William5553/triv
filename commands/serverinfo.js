e77b50a84f068bc64ff51cb068c9359705c351ab
William Kang <contact@williamk.ga>
2022-07-06 21:51:29 -0400

    deps

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,7 +7,6 @@ exports.run = async (client, message) => {
   
     message.channel.send({ embeds: [
       new MessageEmbed()
-        .setColor('RANDOM')
         .setAuthor({ name: message.guild.name, iconURL: message.guild.iconURL({ dynamic: true }) })
         .setFooter({ iconURL: owner.displayAvatarURL({ dynamic: true }), text: `Owner: ${owner.user.tag} | ID: ${message.guild.id}` })
         .addField('Server Created', `<t:${Math.round(message.guild.createdTimestamp / 1000)}:f>`, true)
